6d0d9b8a7e756a06f2cef4751f108d6f3a2bf1f4
Christian Eltzschig <me@elchris.org>
2024-03-21 18:40:56 +0100

    [#162] Add ipc capable trait tests

---
+++ iceoryx2-bb/posix/tests/ipc_capable_trait_tests.rs
@@ -0,0 +1,145 @@
+// Copyright (c) 2024 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache Software License 2.0 which is available at
+// https://www.apache.org/licenses/LICENSE-2.0, or the MIT license
+// which is available at https://opensource.org/licenses/MIT.
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+#[generic_tests::define]
+mod ipc_capable {
+    use iceoryx2_bb_posix::barrier::*;
+    use iceoryx2_bb_posix::ipc_capable::{Handle, HandleState, IpcCapable};
+    use iceoryx2_bb_posix::semaphore::{
+        UnnamedSemaphore, UnnamedSemaphoreBuilder, UnnamedSemaphoreHandle,
+    };
+    use iceoryx2_bb_testing::assert_that;
+
+    trait TestSut {
+        type Handle: Handle;
+        type Sut<'a>: IpcCapable<'a, Self::Handle>;
+
+        fn init_process_local_handle(handle: &Self::Handle);
+        fn init_inter_process_handle(handle: &Self::Handle);
+    }
+
+    #[test]
+    fn new_handle_is_not_initialized<Sut: TestSut>() {
+        let sut_handle = Sut::Handle::new();
+        assert_that!(sut_handle.state(), eq HandleState::Uninitialized);
+    }
+
+    #[test]
+    #[should_panic]
+    fn creating_ipc_construct_from_uninitialized_handle_panics<Sut: TestSut>() {
+        let sut_handle = Sut::Handle::new();
+
+        unsafe { Sut::Sut::from_ipc_handle(&sut_handle) };
+    }
+
+    #[test]
+    #[should_panic]
+    fn creating_ipc_construct_from_process_local_handle_panics<Sut: TestSut>() {
+        let sut_handle = Sut::Handle::new();
+        Sut::init_process_local_handle(&sut_handle);
+
+        unsafe { Sut::Sut::from_ipc_handle(&sut_handle) };
+    }
+
+    #[test]
+    fn creating_ipc_construct_from_ipc_handle_works<Sut: TestSut>() {
+        let sut_handle = Sut::Handle::new();
+        Sut::init_inter_process_handle(&sut_handle);
+
+        // no panic here
+        unsafe { Sut::Sut::from_ipc_handle(&sut_handle) };
+    }
+
+    #[test]
+    #[should_panic]
+    fn init_handle_twice_panics<Sut: TestSut>() {
+        let sut_handle = Sut::Handle::new();
+        Sut::init_process_local_handle(&sut_handle);
+
+        Sut::init_inter_process_handle(&sut_handle);
+    }
+
+    #[test]
+    fn initialized_handle_is_initialized<Sut: TestSut>() {
+        let sut_handle_1 = Sut::Handle::new();
+        let sut_handle_2 = Sut::Handle::new();
+
+        assert_that!(sut_handle_1.state(), eq HandleState::Uninitialized);
+        assert_that!(sut_handle_2.state(), eq HandleState::Uninitialized);
+
+        Sut::init_process_local_handle(&sut_handle_1);
+        Sut::init_inter_process_handle(&sut_handle_2);
+
+        assert_that!(sut_handle_1.state(), eq HandleState::Initialized);
+        assert_that!(sut_handle_2.state(), eq HandleState::Initialized);
+    }
+
+    #[test]
+    fn inter_process_capability_is_set_correctly<Sut: TestSut>() {
+        let sut_handle_1 = Sut::Handle::new();
+        let sut_handle_2 = Sut::Handle::new();
+
+        Sut::init_process_local_handle(&sut_handle_1);
+        Sut::init_inter_process_handle(&sut_handle_2);
+
+        assert_that!(sut_handle_1.is_inter_process_capable(), eq false);
+        assert_that!(sut_handle_2.is_inter_process_capable(), eq true);
+    }
+
+    struct BarrierTest {}
+
+    impl TestSut for BarrierTest {
+        type Handle = BarrierHandle;
+        type Sut<'a> = Barrier<'a>;
+
+        fn init_process_local_handle(handle: &Self::Handle) {
+            BarrierBuilder::new(1)
+                .is_interprocess_capable(false)
+                .create(handle)
+                .unwrap();
+        }
+
+        fn init_inter_process_handle(handle: &Self::Handle) {
+            BarrierBuilder::new(1)
+                .is_interprocess_capable(true)
+                .create(handle)
+                .unwrap();
+        }
+    }
+
+    struct UnnamedSemaphoreTest {}
+
+    impl TestSut for UnnamedSemaphoreTest {
+        type Handle = UnnamedSemaphoreHandle;
+        type Sut<'a> = UnnamedSemaphore<'a>;
+
+        fn init_process_local_handle(handle: &Self::Handle) {
+            UnnamedSemaphoreBuilder::new()
+                .is_interprocess_capable(false)
+                .create(handle)
+                .unwrap();
+        }
+
+        fn init_inter_process_handle(handle: &Self::Handle) {
+            UnnamedSemaphoreBuilder::new()
+                .is_interprocess_capable(true)
+                .create(handle)
+                .unwrap();
+        }
+    }
+
+    #[instantiate_tests(<BarrierTest>)]
+    mod barrier {}
+
+    #[instantiate_tests(<UnnamedSemaphoreTest>)]
+    mod unnamed_semaphore {}
+}
