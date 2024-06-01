70738859ef5f6df1fed877bdbd5508c445f84e70
Mathias Kraus <elboberido@m-hias.de>
2024-06-01 17:06:04 +0200

    [#210] Let discovery C example print services

--- iceoryx2-lang/c/src/lib.rs
+++ iceoryx2-lang/c/src/lib.rs
@@ -1,4 +1,18 @@
+use iceoryx2::prelude::*;
+
 #[no_mangle]
-pub extern "C" fn zero_copy_service_list() {
-    println!("hello world");
+pub extern "C" fn zero_copy_service_list() -> i32 {
+    let services = zero_copy::Service::list();
+
+    if services.is_err() {
+        return -1;
+    }
+
+    let services = services.unwrap();
+
+    for service in services {
+        println!("\n{:#?}", &service);
+    }
+
+    return 0;
 }
