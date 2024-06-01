1ed778455fbe3722ce09b4d0ead17337281a9222
Mathias Kraus <elboberido@m-hias.de>
2024-06-01 16:35:51 +0200

    [#210] Generate the cbindgen header into the target directory

--- iceoryx2-lang/c/build.rs
+++ iceoryx2-lang/c/build.rs
@@ -1,11 +1,19 @@
 extern crate cbindgen;
 
 use std::env;
+use std::path::Path;
 
 use cbindgen::Config;
 
 fn main() {
-    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    // this is the out dir of the iceoryx2-lang C crate not the workspace out dir,
+    // therefore we need to traverse to a known location and create the path for the header
+    let out_dir = env::var("OUT_DIR").expect("Target output directory");
+
+    let mut header_path = Path::new(&out_dir).join("../../../").canonicalize().expect("Path to iceoryx2 base dir for header generation");
+    header_path.push("iceoryx2_lang_c_cbindgen/include/iox2/iceoryx2.h");
+
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").expect("Cargo manifest dir");
 
     let mut config = Config::default();
     config.language = cbindgen::Language::C;
@@ -15,5 +23,5 @@ fn main() {
         .with_config(config)
         .generate()
         .expect("Unable to generate c bindings")
-        .write_to_file("iceoryx2.h");
+        .write_to_file(header_path);
 }
