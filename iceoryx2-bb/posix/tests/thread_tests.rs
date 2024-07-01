d0e9c019a89251f50e1917bc7382a1ede25342bf
Mathias Kraus <elboberido@m-hias.de>
2024-07-01 22:18:54 +0200

    [#210] Add more tests to thread

--- iceoryx2-bb/posix/tests/thread_tests.rs
+++ iceoryx2-bb/posix/tests/thread_tests.rs
@@ -14,7 +14,7 @@ use iceoryx2_bb_posix::thread::*;
 use iceoryx2_bb_testing::assert_that;
 use std::sync::Arc;
 use std::sync::Barrier;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 #[test]
 fn thread_set_name_works() {
@@ -158,6 +158,40 @@ fn thread_set_affinity_from_thread_works() {
 }
 
 #[test]
+fn thread_destructor_does_not_block_on_empty_thread() {
+    let start = Instant::now();
+    {
+        let _thread = {
+            ThreadBuilder::new()
+                .spawn(|| {
+                    // nothing to see, move along
+                })
+                .unwrap()
+        };
+    }
+    assert_that!(start.elapsed(), lt(Duration::from_millis(10)));
+}
+
+#[test]
+fn thread_destructor_does_block_on_busy_thread() {
+    const SLEEP_DURATION: Duration = Duration::from_millis(100);
+    let start = Instant::now();
+    {
+        let _thread = {
+            ThreadBuilder::new()
+                .spawn(|| {
+                    let start = Instant::now();
+                    while start.elapsed() < SLEEP_DURATION {
+                        std::thread::sleep(SLEEP_DURATION - start.elapsed());
+                    }
+                })
+                .unwrap()
+        };
+    }
+    assert_that!(start.elapsed(), gt(SLEEP_DURATION));
+}
+
+#[test]
 fn thread_cancel_works() {
     let barrier = Arc::new(Barrier::new(2));
     let mut thread = {
@@ -187,7 +221,7 @@ fn thread_exit_works() {
             .spawn(move || {
                 // TODO: [#258] This sleep prevents a 'SIGABRT' on Linux with release builds with rustc v1.81.0-nightly!
                 #[cfg(target_os = "linux")]
-                std::thread::sleep(std::time::Duration::from_nanos(1));
+                std::thread::sleep(Duration::from_nanos(1));
                 thread_exit();
                 #[allow(unreachable_code)]
                 {
