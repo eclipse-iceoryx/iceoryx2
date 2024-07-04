df2ba183b9bd1b26503f952f8eef880d2a07aee8
Mathias Kraus <elboberido@m-hias.de>
2024-07-05 01:20:21 +0200

    [#264] Add script to build iceoryx_hoofs on Windows

--- internal/scripts/ci_build_and_install_iceoryx_hoofs.sh
+++ internal/scripts/ci_build_and_install_iceoryx_hoofs.sh
@@ -20,12 +20,19 @@ echo "#######################"
 
 cd $(git rev-parse --show-toplevel)
 
+NUM_JOBS=1
+if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]]; then
+    NUM_JOBS=$(nproc)
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    NUM_JOBS=$(sysctl -n hw.ncpu)
+fi
+
 git clone --depth 1 --branch main https://github.com/eclipse-iceoryx/iceoryx.git target/iceoryx/src
 
 cmake -S target/iceoryx/src/iceoryx_platform -B target/iceoryx/build/platform -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=target/iceoryx/install
-cmake --build target/iceoryx/build/platform
+cmake --build target/iceoryx/build/platform -j$NUM_JOBS
 cmake --install target/iceoryx/build/platform
 
 cmake -S target/iceoryx/src/iceoryx_hoofs -B target/iceoryx/build/hoofs -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=target/iceoryx/install -DCMAKE_PREFIX_PATH="$( pwd )/target/iceoryx/install"
-cmake --build target/iceoryx/build/hoofs
+cmake --build target/iceoryx/build/hoofs -j$NUM_JOBS
 cmake --install target/iceoryx/build/hoofs
