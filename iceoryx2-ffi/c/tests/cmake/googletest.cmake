10f0ada1b0e510a4b60969b0be3838cfcf63b485
Mathias Kraus <elboberido@m-hias.de>
2024-07-05 17:24:00 +0200

    [#264] Use FetchContent_MakeAvailable

--- iceoryx2-ffi/c/tests/cmake/googletest.cmake
+++ iceoryx2-ffi/c/tests/cmake/googletest.cmake
@@ -13,14 +13,14 @@
 include(FetchContent)
 
 FetchContent_Declare(
-  googletest
-  GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG        v1.14.0
-  EXCLUDE_FROM_ALL
+    googletest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG        v1.14.0
+    EXCLUDE_FROM_ALL
 )
 
 FetchContent_GetProperties(googletest)
 if(NOT googletest_POPULATED)
-  FetchContent_Populate(googletest)
-  add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR} EXCLUDE_FROM_ALL)
+    message(STATUS "googletest not found! Using FetchContent!")
 endif()
+FetchContent_MakeAvailable(googletest)
