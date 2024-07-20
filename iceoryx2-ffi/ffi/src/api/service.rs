b27ee91863c1cbe376093e44e6acaed29935f71e
Christian Eltzschig <me@elchris.org>
2024-07-20 22:57:27 +0200

    [#264] Document Service::does_exist

--- iceoryx2-ffi/ffi/src/api/service.rs
+++ iceoryx2-ffi/ffi/src/api/service.rs
@@ -11,17 +11,22 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 #![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 
 // BEGIN type definition
 
 use std::ffi::{c_int, c_void};
 
-use iceoryx2::service::static_config::messaging_pattern;
+use iceoryx2::service::{
+    messaging_pattern::MessagingPattern, process_local, zero_copy, Service, ServiceDetailsError,
+};
 
 use crate::{
-    iox2_callback_progression_e, iox2_config_ptr, iox2_node_name_ptr, iox2_node_name_storage_t,
-    iox2_service_name_ptr, IOX2_OK,
+    iox2_callback_progression_e, iox2_config_ptr, iox2_node_name_ptr, iox2_service_name_ptr,
+    IOX2_OK,
 };
+
+use super::IntoCInt;
 
 #[repr(C)]
 #[derive(Copy, Clone)]
@@ -37,6 +42,15 @@ pub enum iox2_messaging_pattern_e {
     EVENT,
 }
 
+impl From<iox2_messaging_pattern_e> for MessagingPattern {
+    fn from(value: iox2_messaging_pattern_e) -> Self {
+        match value {
+            iox2_messaging_pattern_e::EVENT => MessagingPattern::Event,
+            iox2_messaging_pattern_e::PUBLISH_SUBSCRIBE => MessagingPattern::PublishSubscribe,
+        }
+    }
+}
+
 #[repr(C)]
 #[derive(Copy, Clone)]
 pub enum iox2_service_details_error_e {
@@ -47,6 +61,30 @@ pub enum iox2_service_details_error_e {
     VERSION_MISMATCH,
     INTERNAL_ERROR,
     FAILED_TO_ACQUIRE_NODE_STATE,
+}
+
+impl IntoCInt for ServiceDetailsError {
+    fn into_c_int(self) -> c_int {
+        (match self {
+            ServiceDetailsError::FailedToOpenStaticServiceInfo => {
+                iox2_service_details_error_e::FAILED_TO_OPEN_STATIC_SERVICE_INFO
+            }
+            ServiceDetailsError::FailedToReadStaticServiceInfo => {
+                iox2_service_details_error_e::FAILED_TO_READ_STATIC_SERVICE_INFO
+            }
+            ServiceDetailsError::FailedToDeserializeStaticServiceInfo => {
+                iox2_service_details_error_e::FAILED_TO_DESERIALIZE_STATIC_SERVICE_INFO
+            }
+            ServiceDetailsError::ServiceInInconsistentState => {
+                iox2_service_details_error_e::SERVICE_IN_INCONSISTENT_STATE
+            }
+            ServiceDetailsError::VersionMismatch => iox2_service_details_error_e::VERSION_MISMATCH,
+            ServiceDetailsError::InternalError => iox2_service_details_error_e::INTERNAL_ERROR,
+            ServiceDetailsError::FailedToAcquireNodeState => {
+                iox2_service_details_error_e::FAILED_TO_ACQUIRE_NODE_STATE
+            }
+        }) as c_int
+    }
 }
 
 #[repr(C)]
@@ -62,6 +100,15 @@ pub type iox2_service_list_context = *const c_void;
 
 // END type definition
 
+/// Checks if a specified service exists. If the service exists `does_exist` will contain
+/// true, otherwise false after the call. On error it returns `iox2_service_details_error_e`, on
+/// success `IOX2_OK`.
+///
+/// # Safety
+///
+/// * The `service_name` must be valid and non-null
+/// * The `config` must be valid and non-null
+/// * The `does_exist` must be valid and non-null
 #[no_mangle]
 pub unsafe extern "C" fn iox2_service_does_exist(
     service_type: iox2_service_type_e,
@@ -70,25 +117,48 @@ pub unsafe extern "C" fn iox2_service_does_exist(
     messaging_pattern: iox2_messaging_pattern_e,
     does_exist: *mut bool,
 ) -> c_int {
-    todo!()
+    debug_assert!(!service_name.is_null());
+    debug_assert!(!config.is_null());
+    debug_assert!(!does_exist.is_null());
+
+    let config = &*config;
+    let service_name = &*service_name;
+    let messaging_pattern = messaging_pattern.into();
+
+    let result = match service_type {
+        iox2_service_type_e::IPC => {
+            zero_copy::Service::does_exist(service_name, config, messaging_pattern)
+        }
+        iox2_service_type_e::LOCAL => {
+            process_local::Service::does_exist(service_name, config, messaging_pattern)
+        }
+    };
+
+    match result {
+        Ok(value) => {
+            *does_exist = value;
+            IOX2_OK
+        }
+        Err(e) => e.into_c_int(),
+    }
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn iox2_service_details(
-    service_type: iox2_service_type_e,
-    service_name: iox2_service_name_ptr,
-    config: iox2_config_ptr,
-    messaging_pattern: iox2_messaging_pattern_e,
-    service_details: iox2_node_name_ptr,
+    _service_type: iox2_service_type_e,
+    _service_name: iox2_service_name_ptr,
+    _config: iox2_config_ptr,
+    _messaging_pattern: iox2_messaging_pattern_e,
+    _service_details: iox2_service_name_ptr,
 ) -> c_int {
     todo!()
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn iox2_service_list(
-    config_ptr: iox2_config_ptr,
-    callback: iox2_service_list_callback,
-    callback_ctx: iox2_service_list_context,
+    _config_ptr: iox2_config_ptr,
+    _callback: iox2_service_list_callback,
+    _callback_ctx: iox2_service_list_context,
 ) -> c_int {
     todo!()
 }
