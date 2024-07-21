cdc65aae9ac0d10352ad24c20f2ae3836757818f
Mathias Kraus <elboberido@m-hias.de>
2024-07-21 20:28:59 +0200

    [#210] Create notifier

--- iceoryx2-ffi/ffi/src/api/port_factory_notifier_builder.rs
+++ iceoryx2-ffi/ffi/src/api/port_factory_notifier_builder.rs
@@ -12,16 +12,37 @@
 
 #![allow(non_camel_case_types)]
 
-use crate::api::{iox2_service_type_e, HandleToType};
+use crate::api::{
+    iox2_notifier_h, iox2_notifier_t, iox2_service_type_e, HandleToType, IntoCInt, NotifierUnion,
+    IOX2_OK,
+};
 
+use iceoryx2::port::notifier::NotifierCreateError;
 use iceoryx2::prelude::*;
 use iceoryx2::service::port_factory::notifier::PortFactoryNotifier;
 use iceoryx2_bb_elementary::static_assert::*;
 use iceoryx2_ffi_macros::iceoryx2_ffi;
 
+use core::ffi::c_int;
 use core::mem::ManuallyDrop;
 
 // BEGIN types definition
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub enum iox2_notifier_create_error_e {
+    EXCEEDS_MAX_SUPPORTED_NOTIFIERS = IOX2_OK as isize + 1,
+}
+
+impl IntoCInt for NotifierCreateError {
+    fn into_c_int(self) -> c_int {
+        (match self {
+            NotifierCreateError::ExceedsMaxSupportedNotifiers => {
+                iox2_notifier_create_error_e::EXCEEDS_MAX_SUPPORTED_NOTIFIERS
+            }
+        }) as c_int
+    }
+}
 
 pub(super) union PortFactoryNotifierBuilderUnion {
     ipc: ManuallyDrop<PortFactoryNotifier<'static, zero_copy::Service>>,
@@ -97,5 +118,70 @@ impl HandleToType for iox2_port_factory_notifier_builder_ref_h {
 // END type definition
 
 // BEGIN C API
+
+// TODO [#210] add all the other setter methods
+
+#[no_mangle]
+pub unsafe extern "C" fn iox2_port_factory_notifier_builder_create(
+    port_factory_handle: iox2_port_factory_notifier_builder_ref_h,
+    notifier_struct_ptr: *mut iox2_notifier_t,
+    notifier_handle_ptr: *mut iox2_notifier_h,
+) -> c_int {
+    debug_assert!(!port_factory_handle.is_null());
+    debug_assert!(!notifier_handle_ptr.is_null());
+
+    let mut notifier_struct_ptr = notifier_struct_ptr;
+    fn no_op(_: *mut iox2_notifier_t) {}
+    let mut deleter: fn(*mut iox2_notifier_t) = no_op;
+    if notifier_struct_ptr.is_null() {
+        notifier_struct_ptr = iox2_notifier_t::alloc();
+        deleter = iox2_notifier_t::dealloc;
+    }
+    debug_assert!(!notifier_struct_ptr.is_null());
+
+    let notifier_builder_struct = unsafe { &mut *port_factory_handle.as_type() };
+
+    let service_type = notifier_builder_struct.service_type;
+    match service_type {
+        iox2_service_type_e::IPC => {
+            let notifier_builder =
+                ManuallyDrop::take(&mut notifier_builder_struct.value.as_mut().ipc);
+
+            match notifier_builder.create() {
+                Ok(notifier) => {
+                    (*notifier_struct_ptr).init(
+                        service_type,
+                        NotifierUnion::new_ipc(notifier),
+                        deleter,
+                    );
+                }
+                Err(error) => {
+                    return error.into_c_int();
+                }
+            }
+        }
+        iox2_service_type_e::LOCAL => {
+            let notifier_builder =
+                ManuallyDrop::take(&mut notifier_builder_struct.value.as_mut().local);
+
+            match notifier_builder.create() {
+                Ok(notifier) => {
+                    (*notifier_struct_ptr).init(
+                        service_type,
+                        NotifierUnion::new_local(notifier),
+                        deleter,
+                    );
+                }
+                Err(error) => {
+                    return error.into_c_int();
+                }
+            }
+        }
+    }
+
+    *notifier_handle_ptr = (*notifier_struct_ptr).as_handle();
+
+    IOX2_OK
+}
 
 // END C API
