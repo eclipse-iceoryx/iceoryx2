4a9abf1050764584be74af967268ebee74c59dee
Mathias Kraus <elboberido@m-hias.de>
2024-07-24 23:20:20 +0200

    [#210] Create publisher

--- iceoryx2-ffi/ffi/src/api/port_factory_publisher_builder.rs
+++ iceoryx2-ffi/ffi/src/api/port_factory_publisher_builder.rs
@@ -12,16 +12,41 @@
 
 #![allow(non_camel_case_types)]
 
-use crate::api::{iox2_service_type_e, HandleToType, NoUserHeaderFfi, PayloadFfi};
+use crate::api::{
+    c_size_t, iox2_publisher_h, iox2_publisher_t, iox2_service_type_e, HandleToType, IntoCInt,
+    NoUserHeaderFfi, PayloadFfi, PublisherUnion, IOX2_OK,
+};
 
+use iceoryx2::port::publisher::PublisherCreateError;
 use iceoryx2::prelude::*;
 use iceoryx2::service::port_factory::publisher::PortFactoryPublisher;
 use iceoryx2_bb_elementary::static_assert::*;
 use iceoryx2_ffi_macros::iceoryx2_ffi;
 
+use core::ffi::c_int;
 use core::mem::ManuallyDrop;
 
 // BEGIN types definition
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub enum iox2_publisher_create_error_e {
+    EXCEEDS_MAX_SUPPORTED_PUBLISHERS = IOX2_OK as isize + 1,
+    UNABLE_TO_CREAT_DATA_SEGMENT,
+}
+
+impl IntoCInt for PublisherCreateError {
+    fn into_c_int(self) -> c_int {
+        (match self {
+            PublisherCreateError::ExceedsMaxSupportedPublishers => {
+                iox2_publisher_create_error_e::EXCEEDS_MAX_SUPPORTED_PUBLISHERS
+            }
+            PublisherCreateError::UnableToCreateDataSegment => {
+                iox2_publisher_create_error_e::UNABLE_TO_CREAT_DATA_SEGMENT
+            }
+        }) as c_int
+    }
+}
 
 pub(super) union PortFactoryPublisherBuilderUnion {
     ipc: ManuallyDrop<
@@ -112,3 +137,146 @@ impl HandleToType for iox2_port_factory_publisher_builder_ref_h {
 }
 
 // END type definition
+
+// BEGIN C API
+
+/// This function casts an owning [`iox2_port_factory_publisher_builder_h`] into a non-owning [`iox2_port_factory_publisher_builder_ref_h`]
+///
+/// # Arguments
+///
+/// * `port_factory_handle` obtained by [`iox2_port_factory_pub_sub_publisher_builder`](crate::iox2_port_factory_pub_sub_publisher_builder)
+///
+/// Returns a [`iox2_port_factory_publisher_builder_ref_h`]
+///
+/// # Safety
+///
+/// * The `port_factory_handle` must be a valid handle.
+/// * The `port_factory_handle` is still valid after the call to this function.
+#[no_mangle]
+pub unsafe extern "C" fn iox2_cast_port_factory_publisher_builder_ref_h(
+    port_factory_handle: iox2_port_factory_publisher_builder_h,
+) -> iox2_port_factory_publisher_builder_ref_h {
+    debug_assert!(!port_factory_handle.is_null());
+
+    (*port_factory_handle.as_type()).as_ref_handle() as *mut _ as _
+}
+
+/// Sets the max loaned samples for the publisher
+///
+/// # Arguments
+///
+/// * `port_factory_handle` - Must be a valid [`iox2_port_factory_publisher_builder_ref_h`]
+///   obtained by [`iox2_port_factory_pub_sub_publisher_builder`](crate::iox2_port_factory_pub_sub_publisher_builder) and
+///   casted by [`iox2_cast_port_factory_publisher_builder_ref_h`].
+/// * `value` - The value to set max loaned samples to
+///
+/// # Safety
+///
+/// * `port_factory_handle` must be valid handles
+#[no_mangle]
+pub unsafe extern "C" fn iox2_port_factory_publisher_builder_set_max_loaned_samples(
+    port_factory_handle: iox2_port_factory_publisher_builder_ref_h,
+    value: c_size_t,
+) {
+    debug_assert!(!port_factory_handle.is_null());
+
+    let port_factory_struct = unsafe { &mut *port_factory_handle.as_type() };
+    match port_factory_struct.service_type {
+        iox2_service_type_e::IPC => {
+            let port_factory = ManuallyDrop::take(&mut port_factory_struct.value.as_mut().ipc);
+
+            port_factory_struct.set(PortFactoryPublisherBuilderUnion::new_ipc(
+                port_factory.max_loaned_samples(value),
+            ));
+        }
+        iox2_service_type_e::LOCAL => {
+            let port_factory = ManuallyDrop::take(&mut port_factory_struct.value.as_mut().local);
+
+            port_factory_struct.set(PortFactoryPublisherBuilderUnion::new_local(
+                port_factory.max_loaned_samples(value),
+            ));
+        }
+    }
+}
+
+// TODO [#210] add all the other setter methods
+
+/// Creates a publisher and consumes the builder
+///
+/// # Arguments
+///
+/// * `port_factory_handle` - Must be a valid [`iox2_port_factory_publisher_builder_h`] obtained by [`iox2_port_factory_pub_sub_publisher_builder`](crate::iox2_port_factory_pub_sub_publisher_builder).
+/// * `publisher_struct_ptr` - Must be either a NULL pointer or a pointer to a valid [`iox2_publisher_t`]. If it is a NULL pointer, the storage will be allocated on the heap.
+/// * `publisher_handle_ptr` - An uninitialized or dangling [`iox2_publisher_h`] handle which will be initialized by this function call.
+///
+/// Returns IOX2_OK on success, an [`iox2_publisher_create_error_e`] otherwise.
+///
+/// # Safety
+///
+/// * The `port_factory_handle` is invalid after the return of this function and leads to undefined behavior if used in another function call!
+/// * The corresponding [`iox2_port_factory_publisher_builder_t`]
+///   can be re-used with a call to  [`iox2_port_factory_pub_sub_publisher_builder`](crate::iox2_port_factory_pub_sub_publisher_builder)!
+#[no_mangle]
+pub unsafe extern "C" fn iox2_port_factory_publisher_builder_create(
+    port_factory_handle: iox2_port_factory_publisher_builder_h,
+    publisher_struct_ptr: *mut iox2_publisher_t,
+    publisher_handle_ptr: *mut iox2_publisher_h,
+) -> c_int {
+    debug_assert!(!port_factory_handle.is_null());
+    debug_assert!(!publisher_handle_ptr.is_null());
+
+    let mut publisher_struct_ptr = publisher_struct_ptr;
+    fn no_op(_: *mut iox2_publisher_t) {}
+    let mut deleter: fn(*mut iox2_publisher_t) = no_op;
+    if publisher_struct_ptr.is_null() {
+        publisher_struct_ptr = iox2_publisher_t::alloc();
+        deleter = iox2_publisher_t::dealloc;
+    }
+    debug_assert!(!publisher_struct_ptr.is_null());
+
+    let publisher_builder_struct = unsafe { &mut *port_factory_handle.as_type() };
+
+    let service_type = publisher_builder_struct.service_type;
+    match service_type {
+        iox2_service_type_e::IPC => {
+            let publisher_builder =
+                ManuallyDrop::take(&mut publisher_builder_struct.value.as_mut().ipc);
+
+            match publisher_builder.create() {
+                Ok(publisher) => {
+                    (*publisher_struct_ptr).init(
+                        service_type,
+                        PublisherUnion::new_ipc(publisher),
+                        deleter,
+                    );
+                }
+                Err(error) => {
+                    return error.into_c_int();
+                }
+            }
+        }
+        iox2_service_type_e::LOCAL => {
+            let publisher_builder =
+                ManuallyDrop::take(&mut publisher_builder_struct.value.as_mut().local);
+
+            match publisher_builder.create() {
+                Ok(publisher) => {
+                    (*publisher_struct_ptr).init(
+                        service_type,
+                        PublisherUnion::new_local(publisher),
+                        deleter,
+                    );
+                }
+                Err(error) => {
+                    return error.into_c_int();
+                }
+            }
+        }
+    }
+
+    *publisher_handle_ptr = (*publisher_struct_ptr).as_handle();
+
+    IOX2_OK
+}
+
+// END C API
--- iceoryx2-ffi/ffi/src/api/publisher.rs
+++ iceoryx2-ffi/ffi/src/api/publisher.rs
@@ -10,8 +10,127 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-use core::time::Duration;
+#![allow(non_camel_case_types)]
+
+use crate::api::{iox2_service_type_e, HandleToType, NoUserHeaderFfi, PayloadFfi};
+
+use iceoryx2::port::publisher::Publisher;
 use iceoryx2::prelude::*;
+use iceoryx2_bb_elementary::static_assert::*;
+use iceoryx2_ffi_macros::iceoryx2_ffi;
+
+use core::mem::ManuallyDrop;
+
+// BEGIN types definition
+
+pub(super) union PublisherUnion {
+    ipc: ManuallyDrop<Publisher<zero_copy::Service, PayloadFfi, NoUserHeaderFfi>>,
+    local: ManuallyDrop<Publisher<process_local::Service, PayloadFfi, NoUserHeaderFfi>>,
+}
+
+impl PublisherUnion {
+    pub(super) fn new_ipc(
+        publisher: Publisher<zero_copy::Service, PayloadFfi, NoUserHeaderFfi>,
+    ) -> Self {
+        Self {
+            ipc: ManuallyDrop::new(publisher),
+        }
+    }
+    pub(super) fn new_local(
+        publisher: Publisher<process_local::Service, PayloadFfi, NoUserHeaderFfi>,
+    ) -> Self {
+        Self {
+            local: ManuallyDrop::new(publisher),
+        }
+    }
+}
+
+#[repr(C)]
+#[repr(align(16))] // alignment of Option<PublisherUnion>
+pub struct iox2_publisher_storage_t {
+    internal: [u8; 40], // magic number obtained with size_of::<Option<PublisherUnion>>()
+}
+
+#[repr(C)]
+#[iceoryx2_ffi(PublisherUnion)]
+pub struct iox2_publisher_t {
+    service_type: iox2_service_type_e,
+    value: iox2_publisher_storage_t,
+    deleter: fn(*mut iox2_publisher_t),
+}
+
+impl iox2_publisher_t {
+    pub(super) fn init(
+        &mut self,
+        service_type: iox2_service_type_e,
+        value: PublisherUnion,
+        deleter: fn(*mut iox2_publisher_t),
+    ) {
+        self.service_type = service_type;
+        self.value.init(value);
+        self.deleter = deleter;
+    }
+}
+
+pub struct iox2_publisher_h_t;
+/// The owning handle for `iox2_publisher_t`. Passing the handle to an function transfers the ownership.
+pub type iox2_publisher_h = *mut iox2_publisher_h_t;
+
+pub struct iox2_publisher_ref_h_t;
+/// The non-owning handle for `iox2_publisher_t`. Passing the handle to an function does not transfers the ownership.
+pub type iox2_publisher_ref_h = *mut iox2_publisher_ref_h_t;
+
+impl HandleToType for iox2_publisher_h {
+    type Target = *mut iox2_publisher_t;
+
+    fn as_type(self) -> Self::Target {
+        self as *mut _ as _
+    }
+}
+
+impl HandleToType for iox2_publisher_ref_h {
+    type Target = *mut iox2_publisher_t;
+
+    fn as_type(self) -> Self::Target {
+        self as *mut _ as _
+    }
+}
+
+// END type definition
+
+// BEGIN C API
+
+/// This function needs to be called to destroy the publisher!
+///
+/// # Arguments
+///
+/// * `publisher_handle` - A valid [`iox2_publisher_h`]
+///
+/// # Safety
+///
+/// * The `publisher_handle` is invalid after the return of this function and leads to undefined behavior if used in another function call!
+/// * The corresponding [`iox2_publisher_t`] can be re-used with a call to
+///   [`iox2_port_factory_publisher_builder_create`](crate::iox2_port_factory_publisher_builder_create)!
+#[no_mangle]
+pub unsafe extern "C" fn iox2_publisher_drop(publisher_handle: iox2_publisher_h) {
+    debug_assert!(!publisher_handle.is_null());
+
+    let publisher = &mut *publisher_handle.as_type();
+
+    match publisher.service_type {
+        iox2_service_type_e::IPC => {
+            ManuallyDrop::drop(&mut publisher.value.as_mut().ipc);
+        }
+        iox2_service_type_e::LOCAL => {
+            ManuallyDrop::drop(&mut publisher.value.as_mut().local);
+        }
+    }
+    (publisher.deleter)(publisher);
+}
+
+// END C API
+
+use core::time::Duration;
 use iceoryx2_bb_log::set_log_level;
 
 const CYCLE_TIME: Duration = Duration::from_secs(1);
