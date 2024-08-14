5fec6e578a5cb5019dbf5c2a1f2c26493d3d13f0
Christian Eltzschig <me@elchris.org>
2024-08-14 17:32:09 +0200

    [#264] Define port id headers

--- iceoryx2-ffi/cxx/include/iox2/unique_port_id.hpp
+++ iceoryx2-ffi/cxx/include/iox2/unique_port_id.hpp
@@ -18,17 +18,76 @@
 namespace iox2 {
 class UniquePublisherId {
   public:
+    UniquePublisherId(const UniquePublisherId&) = delete;
+    UniquePublisherId(UniquePublisherId&& rhs) = delete;
+    auto operator=(const UniquePublisherId& rhs) noexcept -> UniquePublisherId&;
+    auto operator=(UniquePublisherId&& rhs) noexcept -> UniquePublisherId&;
+    ~UniquePublisherId();
+
     auto operator==(const UniquePublisherId& rhs) -> bool;
     auto operator<(const UniquePublisherId& rhs) -> bool;
 
   private:
     friend class HeaderPublishSubscribe;
     explicit UniquePublisherId(iox2_unique_publisher_id_h handle);
+    void drop();
+
     iox2_unique_publisher_id_h m_handle;
 };
-class UniqueSubscriberId { };
-class UniqueNotifierId { };
-class UniqueListenerId { };
+
+class UniqueSubscriberId {
+  public:
+    UniqueSubscriberId(const UniqueSubscriberId&) = delete;
+    UniqueSubscriberId(UniqueSubscriberId&& rhs) = delete;
+    auto operator=(const UniqueSubscriberId& rhs) noexcept -> UniqueSubscriberId&;
+    auto operator=(UniqueSubscriberId&& rhs) noexcept -> UniqueSubscriberId&;
+    ~UniqueSubscriberId();
+
+    auto operator==(const UniqueSubscriberId& rhs) -> bool;
+    auto operator<(const UniqueSubscriberId& rhs) -> bool;
+
+  private:
+    explicit UniqueSubscriberId(iox2_unique_subscriber_id_h handle);
+    void drop();
+
+    iox2_unique_subscriber_id_h m_handle;
+};
+
+class UniqueNotifierId {
+  public:
+    UniqueNotifierId(const UniqueNotifierId&) = delete;
+    UniqueNotifierId(UniqueNotifierId&& rhs) = delete;
+    auto operator=(const UniqueNotifierId& rhs) noexcept -> UniqueNotifierId&;
+    auto operator=(UniqueNotifierId&& rhs) noexcept -> UniqueNotifierId&;
+    ~UniqueNotifierId();
+
+    auto operator==(const UniqueNotifierId& rhs) -> bool;
+    auto operator<(const UniqueNotifierId& rhs) -> bool;
+
+  private:
+    explicit UniqueNotifierId(iox2_unique_notifier_id_h handle);
+    void drop();
+
+    iox2_unique_notifier_id_h m_handle;
+};
+
+class UniqueListenerId {
+  public:
+    UniqueListenerId(const UniqueListenerId&) = delete;
+    UniqueListenerId(UniqueListenerId&& rhs) = delete;
+    auto operator=(const UniqueListenerId& rhs) noexcept -> UniqueListenerId&;
+    auto operator=(UniqueListenerId&& rhs) noexcept -> UniqueListenerId&;
+    ~UniqueListenerId();
+
+    auto operator==(const UniqueListenerId& rhs) -> bool;
+    auto operator<(const UniqueListenerId& rhs) -> bool;
+
+  private:
+    explicit UniqueListenerId(iox2_unique_listener_id_h handle);
+    void drop();
+
+    iox2_unique_listener_id_h m_handle;
+};
 } // namespace iox2
 
 #endif
