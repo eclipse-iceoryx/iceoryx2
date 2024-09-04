73a5919ac7d4f99e8508840d1f2c624df8ffab0b
Jeff Ithier <jeff@ithier.dev>
2024-09-04 14:03:58 +0200

    [#98] Move help template to utils library

--- iceoryx2-cli/iox2/src/cli.rs
+++ iceoryx2-cli/iox2/src/cli.rs
@@ -11,26 +11,27 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 use clap::Parser;
-use colored::*;
+
+use iceoryx2_cli_utils::help_template;
 
 #[derive(Parser, Debug)]
 #[command(
     name = "iox2",
-    about = "The command-line interface to iceoryx2",
+    about = "The command-line interface entrypoint to iceoryx2.",
     long_about = None,
     version = env!("CARGO_PKG_VERSION"),
     disable_help_subcommand = true,
-    arg_required_else_help = true,
-    help_template = help_template(),
+    arg_required_else_help = false,
+    help_template = help_template("iox2", true),
 )]
 pub struct Cli {
-    #[arg(short, long, help = "List all installed commands")]
+    #[arg(short, long, help = "List all installed external commands")]
     pub list: bool,
 
     #[arg(
         short,
         long,
-        help = "Display paths that will be checked for installed commands"
+        help = "Display paths that will be checked for external commands"
     )]
     pub paths: bool,
 
@@ -48,17 +49,4 @@ pub struct Cli {
         allow_hyphen_values = true
     )]
     pub external_command: Vec<String>,
-}
-
-fn help_template() -> String {
-    format!(
-        "{}{}{}\n\n{}\n{{options}}\n\n{}\n{{subcommands}}{}{}",
-        "Usage: ".bright_green().bold(),
-        "iox2 ".bold(),
-        "[OPTIONS] [COMMAND]",
-        "Options:".bright_green().bold(),
-        "Commands:".bright_green().bold(),
-        "  ...         ".bold(),
-        "See all installed commands with --list"
-    )
 }
