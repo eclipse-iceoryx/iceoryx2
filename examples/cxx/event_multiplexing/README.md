1ebb726432236e5eb0fd5dc7fb123c5802da757a
Christian Eltzschig <me@elchris.org>
2024-10-08 18:00:43 +0200

    [#390] Add C++ example and implement notifier

---
+++ examples/cxx/event_multiplexing/README.md
@@ -0,0 +1,56 @@
+# Event
+
+Before proceeding, all dependencies need to be installed. You can find
+instructions in the [C++ Examples Readme](../README.md).
+
+## Running The Example
+
+This example demonstrates iceoryx2's event multiplexing mechanism,
+called the `WaitSet`. It allows waiting, with a single call, on
+multiple `Listener` ports as well as external file descriptor-based
+events such as `sockets`.
+
+In this setup, the `wait` process monitors an arbitrary number of
+services, which the user can specify via the command line option `-s`.
+The `notifier` can define the service to which it will send event
+notifications using the `-s` option and specify the event ID with
+the `-e` option.
+
+In the example below, we are waiting for events on the services `fuu` and
+`bar`. Service `fuu` is notified with event ID `123`, and service `bar` is
+notified with event ID `456`.
+
+### Terminal 1
+
+```sh
+cargo run --example event_multiplexing_wait -- -s "fuu" -s "bar"
+```
+
+### Terminal 2
+
+```sh
+cargo run --example event_multiplexing_notifier -- -s "fuu" -e 123
+```
+
+### Terminal 3
+
+```sh
+cargo run --example event_multiplexing_notifier -- -s "bar" -e 456
+```
+
+Feel free to instantiate multiple notifiers for the same service with the same
+or different event id's. Or to for different services.
+
+## Technical Details
+
+The `WaitSet` utilizes `epoll`, `select`, or other event-multiplexing
+mechanisms. Before the `WaitSet` can monitor a specific event, it must first be
+attached using `WaitSet::attach()`, which returns a RAII `Guard`. This `Guard`
+automatically detaches the attachment when it goes out of scope.
+
+The `WaitSet::**_wait()` calls require a closure that is invoked for each
+triggered attachment and provides the `AttachmentId`. The user can either use
+`AttachmentId::originates_from($ATTACHED_OBJECT$)` to identify the object
+associated with the `AttachmentId`, or set up a
+`HashMap::<AttachmentId, Listener<ipc::Service>>` to quickly access the
+corresponding object.
---
+++ examples/cxx/event_multiplexing/src/notifier.cpp
@@ -0,0 +1,59 @@
+// Copyright (c) 2024 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache Software License 2.0 which is available at
+// https://www.apache.org/licenses/LICENSE-2.0, or the MIT license
+// which is available at https://opensource.org/licenses/MIT.
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+#include <iostream>
+
+#include "iox/cli_definition.hpp"
+#include "iox/duration.hpp"
+#include "iox2/event_id.hpp"
+#include "iox2/node.hpp"
+#include "iox2/service_name.hpp"
+#include "iox2/service_type.hpp"
+
+constexpr iox::units::Duration CYCLE_TIME = iox::units::Duration::fromSeconds(1);
+
+// NOLINTBEGIN
+struct Args {
+    IOX_CLI_DEFINITION(Args);
+    IOX_CLI_OPTIONAL(iox::string<256>, service, { "my_funky_service" }, 's', "service", "The name of the service.");
+    IOX_CLI_OPTIONAL(
+        uint64_t, event_id, { 0 }, 'e', "event_id", "The event id that shall be used to trigger the service.");
+};
+// NOLINTEND
+
+
+auto main(int argc, char** argv) -> int {
+    using namespace iox2;
+    auto args = Args::parse(argc, argv, "Notifier of the event multiplexing example.");
+
+    auto event_id = EventId(args.event_id());
+    auto service_name = ServiceName::create(args.service().c_str()).expect("valid service name");
+
+    auto node = NodeBuilder().create<ServiceType::Ipc>().expect("successful node creation");
+
+    auto service =
+        node.service_builder(service_name).event().open_or_create().expect("successful service creation/opening");
+    auto max_event_id = service.static_config().event_id_max_value();
+
+    auto notifier = service.notifier_builder().create().expect("successful notifier creation");
+
+    while (node.wait(CYCLE_TIME).has_value()) {
+        notifier.notify_with_custom_event_id(event_id).expect("notification");
+
+        std::cout << "[service: \"" << service_name.to_string().c_str() << "\"] Trigger event with id " << event_id
+                  << "..." << std::endl;
+    }
+
+    std::cout << "exit" << std::endl;
+
+    return 0;
+}
