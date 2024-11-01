8229d3285c8bc7bfa5c74f6de2df560606e52182
Christian Eltzschig <me@elchris.org>
2024-11-01 11:40:20 +0100

    [#497] Introduce iceoryx2-fal, the framework abstraction layer

--- internal/scripts/crates_io_publish_script.sh
+++ internal/scripts/crates_io_publish_script.sh
@@ -31,6 +31,7 @@ cargo publish -p iceoryx2-bb-threadsafe
 cargo publish -p iceoryx2-bb-memory
 cargo publish -p iceoryx2-cal
 cargo publish -p iceoryx2-bb-trait-tests
+cargo publish -p iceoryx2-fal
 cargo publish -p iceoryx2
 cargo publish -p iceoryx2-ffi-macros
 cargo publish -p iceoryx2-ffi
