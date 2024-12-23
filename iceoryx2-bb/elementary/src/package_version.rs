0c7ee82e491111ca33908ec76c2f54cf26c2cc74
Christian Eltzschig <me@elchris.org>
2024-12-23 16:05:44 +0100

    [#77] Update to v0.5

--- iceoryx2-bb/elementary/src/package_version.rs
+++ iceoryx2-bb/elementary/src/package_version.rs
@@ -63,8 +63,8 @@ impl PackageVersion {
     /// Returns the current [`PackageVersion`]
     pub fn get() -> PackageVersion {
         const MAJOR: u16 = 0;
-        const MINOR: u16 = 4;
-        const PATCH: u16 = 1;
+        const MINOR: u16 = 5;
+        const PATCH: u16 = 0;
 
         PackageVersion::from_version(MAJOR, MINOR, PATCH)
     }
