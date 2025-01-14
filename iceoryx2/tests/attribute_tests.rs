aa4eb1d0853e8ea431f146b19aed0bfcc9065db7
Christian Eltzschig <me@elchris.org>
2025-01-14 12:01:16 +0100

    [#590] Add attribute accessor tests

--- iceoryx2/tests/attribute_tests.rs
+++ iceoryx2/tests/attribute_tests.rs
@@ -13,6 +13,7 @@
 #[cfg(test)]
 mod attribute {
     use iceoryx2::service::attribute::AttributeVerifier;
+    use iceoryx2_bb_elementary::CallbackProgression;
     use iceoryx2_bb_testing::assert_that;
 
     #[test]
@@ -34,7 +35,7 @@ mod attribute {
             .require("key_1", "value_2");
 
         assert_that!(sut.attributes().get_key_value_len("key_1"), eq 2);
-        assert_that!(sut.attributes().get_key_value_len("key_2"), eq 2);
+        assert_that!(sut.attributes().get_key_value_len("key_2"), eq 0);
     }
 
     #[test]
@@ -47,5 +48,51 @@ mod attribute {
         assert_that!(sut.attributes().get_key_value_at("another_key", 1), eq Some("another_value_2"));
         assert_that!(sut.attributes().get_key_value_at("another_key", 2), eq None);
         assert_that!(sut.attributes().get_key_value_at("non_existing_key", 0), eq None);
+    }
+
+    #[test]
+    fn attribute_set_get_key_value_works() {
+        let sut = AttributeVerifier::new()
+            .require("wild_ride", "XXL")
+            .require("wild_ride", "S");
+
+        let mut values = vec![];
+        sut.attributes().get_key_values("wild_ride", |v| {
+            values.push(v.to_string());
+            CallbackProgression::Continue
+        });
+
+        assert_that!(values, contains String::from("XXL"));
+        assert_that!(values, contains String::from("S"));
+    }
+
+    #[test]
+    fn attribute_set_get_key_value_stops_on_request() {
+        let sut = AttributeVerifier::new()
+            .require("schwifty", "brothers")
+            .require("schwifty", "sisters");
+
+        let mut counter = 0;
+        sut.attributes().get_key_values("schwifty", |_| {
+            counter += 1;
+            CallbackProgression::Stop
+        });
+
+        assert_that!(counter, eq 1);
+    }
+
+    #[test]
+    fn attribute_set_get_key_value_no_callback_call_when_key_does_not_exist() {
+        let sut = AttributeVerifier::new()
+            .require("schwifler", "brothers")
+            .require("schwifler", "sisters");
+
+        let mut counter = 0;
+        sut.attributes().get_key_values("does not exist", |_| {
+            counter += 1;
+            CallbackProgression::Stop
+        });
+
+        assert_that!(counter, eq 0);
     }
 }
