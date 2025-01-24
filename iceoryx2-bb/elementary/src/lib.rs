d01e9038378f924b0e69fbcd7a68b6adffa1d49b
Christian Eltzschig <me@elchris.org>
2025-01-24 01:03:26 +0100

    [#602] Reduce traits

--- iceoryx2-bb/elementary/src/lib.rs
+++ iceoryx2-bb/elementary/src/lib.rs
@@ -18,11 +18,6 @@
 
 #[macro_use]
 pub mod enum_gen;
-pub mod fixed_size;
-#[macro_use]
-pub mod composable_marker_trait;
-pub mod relocatable;
-pub mod self_contained;
 pub mod shared_memory_safe;
 
 mod as_cstr;
--- iceoryx2-bb/elementary/src/shared_memory_safe.rs
+++ iceoryx2-bb/elementary/src/shared_memory_safe.rs
@@ -10,7 +10,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-use crate::relocatable::Relocatable;
+use iceoryx2_pal_concurrency_sync::iox_atomic::*;
 
 /// Marks types that can be safely stored in shared memory and consumed from multiple process
 /// using different address spaces.
@@ -20,4 +20,89 @@ pub trait SharedMemorySafe {
     }
 }
 
-impl<T: Relocatable> SharedMemorySafe for T {}
+impl SharedMemorySafe for u8 {}
+impl SharedMemorySafe for u16 {}
+impl SharedMemorySafe for u32 {}
+impl SharedMemorySafe for u64 {}
+impl SharedMemorySafe for u128 {}
+
+impl SharedMemorySafe for i8 {}
+impl SharedMemorySafe for i16 {}
+impl SharedMemorySafe for i32 {}
+impl SharedMemorySafe for i64 {}
+impl SharedMemorySafe for i128 {}
+
+impl SharedMemorySafe for f32 {}
+impl SharedMemorySafe for f64 {}
+
+impl SharedMemorySafe for char {}
+impl SharedMemorySafe for bool {}
+
+impl SharedMemorySafe for IoxAtomicU8 {}
+impl SharedMemorySafe for IoxAtomicU16 {}
+impl SharedMemorySafe for IoxAtomicU32 {}
+impl SharedMemorySafe for IoxAtomicU64 {}
+
+impl SharedMemorySafe for IoxAtomicI8 {}
+impl SharedMemorySafe for IoxAtomicI16 {}
+impl SharedMemorySafe for IoxAtomicI32 {}
+impl SharedMemorySafe for IoxAtomicI64 {}
+
+impl<T: SharedMemorySafe> SharedMemorySafe for [T] {}
+impl<T: SharedMemorySafe, const N: usize> SharedMemorySafe for [T; N] {}
+impl<T: SharedMemorySafe> SharedMemorySafe for Option<T> {}
+impl<T: SharedMemorySafe, E: SharedMemorySafe> SharedMemorySafe for Result<T, E> {}
+impl<T: SharedMemorySafe> SharedMemorySafe for core::mem::MaybeUninit<T> {}
+impl<T: SharedMemorySafe> SharedMemorySafe for core::cell::UnsafeCell<T> {}
+
+impl<T1: SharedMemorySafe, T2: SharedMemorySafe> SharedMemorySafe for (T1, T2) {}
+impl<T1: SharedMemorySafe, T2: SharedMemorySafe, T3: SharedMemorySafe> SharedMemorySafe
+    for (T1, T2, T3)
+{
+}
+impl<T1: SharedMemorySafe, T2: SharedMemorySafe, T3: SharedMemorySafe, T4: SharedMemorySafe>
+    SharedMemorySafe for (T1, T2, T3, T4)
+{
+}
+impl<
+        T1: SharedMemorySafe,
+        T2: SharedMemorySafe,
+        T3: SharedMemorySafe,
+        T4: SharedMemorySafe,
+        T5: SharedMemorySafe,
+    > SharedMemorySafe for (T1, T2, T3, T4, T5)
+{
+}
+impl<
+        T1: SharedMemorySafe,
+        T2: SharedMemorySafe,
+        T3: SharedMemorySafe,
+        T4: SharedMemorySafe,
+        T5: SharedMemorySafe,
+        T6: SharedMemorySafe,
+    > SharedMemorySafe for (T1, T2, T3, T4, T5, T6)
+{
+}
+impl<
+        T1: SharedMemorySafe,
+        T2: SharedMemorySafe,
+        T3: SharedMemorySafe,
+        T4: SharedMemorySafe,
+        T5: SharedMemorySafe,
+        T6: SharedMemorySafe,
+        T7: SharedMemorySafe,
+    > SharedMemorySafe for (T1, T2, T3, T4, T5, T6, T7)
+{
+}
+impl<
+        T1: SharedMemorySafe,
+        T2: SharedMemorySafe,
+        T3: SharedMemorySafe,
+        T4: SharedMemorySafe,
+        T5: SharedMemorySafe,
+        T6: SharedMemorySafe,
+        T7: SharedMemorySafe,
+        T8: SharedMemorySafe,
+    > SharedMemorySafe for (T1, T2, T3, T4, T5, T6, T7, T8)
+{
+}
