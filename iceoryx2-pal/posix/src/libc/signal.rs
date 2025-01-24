bb50fa9bb7a644af9369d0173a711e3751a45a27
Mathias Kraus <elboberido@m-hias.de>
2025-01-24 17:34:51 +0100

    [#604] Copy linux platform files to libc platform

---
+++ iceoryx2-pal/posix/src/libc/signal.rs
@@ -0,0 +1,35 @@
+// Copyright (c) 2023 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache Software License 2.0 which is available at
+// https://www.apache.org/licenses/LICENSE-2.0, or the MIT license
+// which is available at https://opensource.org/licenses/MIT.
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+#![allow(non_camel_case_types)]
+#![allow(clippy::missing_safety_doc)]
+use crate::posix::types::*;
+
+pub unsafe fn sigaction(sig: int, act: *const sigaction_t, oact: *mut sigaction_t) -> int {
+    internal::iox2_sigaction_func(sig, act, oact)
+}
+
+pub unsafe fn kill(pid: pid_t, sig: int) -> int {
+    crate::internal::kill(pid, sig)
+}
+
+mod internal {
+    use super::*;
+
+    extern "C" {
+        pub(super) fn iox2_sigaction_func(
+            sig: int,
+            act: *const sigaction_t,
+            oact: *mut sigaction_t,
+        ) -> int;
+    }
+}
