8e8f5fa6ad902253193429cc2f34561f604b78f1
Christian Eltzschig <me@elchris.org>
2025-04-09 20:06:48 +0200

    [#429] First request response test

--- iceoryx2/tests/service_request_response_tests.rs
+++ iceoryx2/tests/service_request_response_tests.rs
@@ -13,9 +13,95 @@
 #[generic_tests::define]
 mod service_request_response {
     use iceoryx2::node::NodeBuilder;
-    use iceoryx2::prelude::*;
+    use iceoryx2::port::client::Client;
+    use iceoryx2::port::server::Server;
+    use iceoryx2::service::port_factory::request_response::PortFactory;
     use iceoryx2::testing::*;
+    use iceoryx2::{pending_response, prelude::*};
     use iceoryx2_bb_testing::assert_that;
+
+    struct Args {
+        number_of_clients: usize,
+        number_of_servers: usize,
+    }
+
+    impl Args {
+        fn new() -> Self {
+            Self {
+                number_of_clients: 1,
+                number_of_servers: 1,
+            }
+        }
+    }
+
+    struct TestFixture<Sut: Service> {
+        _node: Node<Sut>,
+        service: PortFactory<Sut, u64, u64, u64, ()>,
+        clients: Vec<Client<Sut, u64, u64, u64, ()>>,
+        servers: Vec<Server<Sut, u64, u64, u64, ()>>,
+    }
+
+    impl<Sut: Service> TestFixture<Sut> {
+        fn new(args: Args) -> Self {
+            let config = generate_isolated_config();
+            let service_name = generate_service_name();
+            let node = NodeBuilder::new().config(&config).create::<Sut>().unwrap();
+            let service = node
+                .service_builder(&service_name)
+                .request_response::<u64, u64>()
+                .request_user_header::<u64>()
+                .max_servers(args.number_of_servers)
+                .max_clients(args.number_of_clients)
+                .create()
+                .unwrap();
+
+            let mut clients = vec![];
+            for _ in 0..args.number_of_clients {
+                clients.push(service.client_builder().create().unwrap());
+            }
+
+            let mut servers = vec![];
+            for _ in 0..args.number_of_servers {
+                servers.push(service.server_builder().create().unwrap());
+            }
+
+            Self {
+                _node: node,
+                service,
+                clients,
+                servers,
+            }
+        }
+    }
+
+    #[test]
+    fn request_response_stream_works<Sut: Service>() {
+        let test_args = Args {
+            number_of_clients: 5,
+            number_of_servers: 7,
+        };
+        let test = TestFixture::<Sut>::new(test_args);
+
+        let mut pending_responses = vec![];
+        for (n, client) in test.clients.iter().enumerate() {
+            pending_responses.push(client.send_copy(n as _).unwrap());
+        }
+
+        let mut active_responses = vec![];
+        for server in test.servers.iter() {
+            active_responses.push(server.receive().unwrap().unwrap());
+        }
+
+        for (n, active_response) in active_responses.iter().enumerate() {
+            let response_value = n as u64 * *active_response.payload();
+            active_response.send_copy(response_value).unwrap();
+
+            for pending_response in &pending_responses {
+                let response = pending_response.receive().unwrap().unwrap();
+                assert_that!(*response.payload(), eq response_value);
+            }
+        }
+    }
 
     #[test]
     fn communication_with_max_clients_and_servers_works<Sut: Service>() {
