a112e65a40110eac0935f5bc94083c1e23833653
Christian Eltzschig <me@elchris.org>
2025-04-10 18:40:38 +0200

    [#429] Fix data segment size issue for server

--- iceoryx2/tests/service_request_response_tests.rs
+++ iceoryx2/tests/service_request_response_tests.rs
@@ -12,6 +12,8 @@
 
 #[generic_tests::define]
 mod service_request_response {
+    use std::collections::HashSet;
+
     use iceoryx2::node::NodeBuilder;
     use iceoryx2::port::client::Client;
     use iceoryx2::port::server::Server;
@@ -27,6 +29,8 @@ mod service_request_response {
         response_buffer_size: usize,
         request_overflow: bool,
         response_overflow: bool,
+        client_unable_to_deliver_strategy: UnableToDeliverStrategy,
+        server_unable_to_deliver_strategy: UnableToDeliverStrategy,
     }
 
     impl Default for Args {
@@ -37,6 +41,8 @@ mod service_request_response {
                 response_buffer_size: 1,
                 request_overflow: true,
                 response_overflow: true,
+                client_unable_to_deliver_strategy: UnableToDeliverStrategy::DiscardSample,
+                server_unable_to_deliver_strategy: UnableToDeliverStrategy::DiscardSample,
             }
         }
     }
@@ -66,14 +72,26 @@ mod service_request_response {
                 .create()
                 .unwrap();
 
-            let mut clients = vec![];
-            for _ in 0..args.number_of_clients {
-                clients.push(service.client_builder().create().unwrap());
-            }
-
             let mut servers = vec![];
             for _ in 0..args.number_of_servers {
-                servers.push(service.server_builder().create().unwrap());
+                servers.push(
+                    service
+                        .server_builder()
+                        .unable_to_deliver_strategy(args.server_unable_to_deliver_strategy)
+                        .create()
+                        .unwrap(),
+                );
+            }
+
+            let mut clients = vec![];
+            for _ in 0..args.number_of_clients {
+                clients.push(
+                    service
+                        .client_builder()
+                        .unable_to_deliver_strategy(args.client_unable_to_deliver_strategy)
+                        .create()
+                        .unwrap(),
+                );
             }
 
             Self {
@@ -142,6 +160,120 @@ mod service_request_response {
         for n in 0..test_args.response_buffer_size {
             let response = pending_response.receive().unwrap().unwrap();
             assert_that!(*response.payload(), eq test_args.response_buffer_size + n);
+        }
+    }
+
+    #[test]
+    fn response_buffer_size_with_non_overflow_works<Sut: Service>() {
+        let test_args = Args {
+            response_buffer_size: 9,
+            response_overflow: false,
+            server_unable_to_deliver_strategy: UnableToDeliverStrategy::DiscardSample,
+            ..Default::default()
+        };
+
+        let test = TestFixture::<Sut>::new(test_args);
+
+        let pending_response = test.clients[0].send_copy(0).unwrap();
+        let active_request = test.servers[0].receive().unwrap().unwrap();
+
+        for n in 0..test_args.response_buffer_size * 2 {
+            assert_that!(active_request.send_copy(n), is_ok);
+        }
+
+        for n in 0..test_args.response_buffer_size {
+            let response = pending_response.receive().unwrap().unwrap();
+            assert_that!(*response.payload(), eq n);
+        }
+    }
+
+    #[test]
+    fn responses_are_delivered_only_to_the_client_with_the_request<Sut: Service>() {
+        let test_args = Args {
+            number_of_clients: 4,
+            ..Default::default()
+        };
+
+        let test = TestFixture::<Sut>::new(test_args);
+
+        let mut pending_responses = vec![];
+        for (n, client) in test.clients.iter().enumerate() {
+            pending_responses.push(client.send_copy(4 * n + 3).unwrap());
+        }
+
+        let mut active_requests = vec![];
+        for _ in 0..test_args.number_of_clients {
+            active_requests.push(test.servers[0].receive().unwrap().unwrap());
+        }
+
+        for active_request in active_requests {
+            assert_that!(
+                active_request.send_copy(*active_request.payload() + 5),
+                is_ok
+            );
+        }
+
+        for pending_response in pending_responses {
+            let n = *pending_response.payload();
+            assert_that!(*pending_response.receive().unwrap().unwrap(), eq n + 5);
+            assert_that!(pending_response.receive().unwrap(), is_none);
+        }
+    }
+
+    #[test]
+    fn responses_are_delivered_from_all_servers<Sut: Service>() {
+        let test_args = Args {
+            number_of_servers: 3,
+            ..Default::default()
+        };
+
+        let test = TestFixture::<Sut>::new(test_args);
+
+        let pending_responses = test.clients[0].send_copy(0).unwrap();
+
+        let mut server_ids = HashSet::new();
+        let mut active_requests = vec![];
+        for n in 0..test_args.number_of_servers {
+            active_requests.push(test.servers[n].receive().unwrap().unwrap());
+            server_ids.insert(test.servers[n].id());
+        }
+
+        for active_request in active_requests {
+            assert_that!(active_request.send_copy(0), is_ok);
+        }
+
+        for _ in 0..test_args.number_of_servers {
+            let response = pending_responses.receive().unwrap().unwrap();
+            assert_that!(server_ids, contains response.origin());
+            server_ids.remove(&response.origin());
+        }
+    }
+
+    #[test]
+    fn responses_from_previous_requests_are_filtered_out<Sut: Service>() {
+        const ITERATIONS: usize = 19;
+        let test_args = Args {
+            response_buffer_size: 9,
+            response_overflow: true,
+            ..Default::default()
+        };
+
+        let test = TestFixture::<Sut>::new(test_args);
+
+        for i in 0..ITERATIONS {
+            let pending_response = test.clients[0].send_copy(0).unwrap();
+            let active_request = test.servers[0].receive().unwrap().unwrap();
+
+            // fill full buffer
+            for n in 0..test_args.response_buffer_size {
+                assert_that!(active_request.send_copy(n * i), is_ok);
+            }
+
+            // just grab half of the buffer, the remainders must be cleaned up
+            for n in 0..test_args.response_buffer_size / 2 {
+                let response = pending_response.receive().unwrap().unwrap();
+                assert_that!(*response, eq n * i);
+            }
         }
     }
 
