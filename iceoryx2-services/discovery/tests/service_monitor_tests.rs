dbb7b9e6773f382b9bfd2687ca43aa31048a47c3
Jeff Ithier <jeff@ithier.dev>
2025-04-22 16:03:43 +0200

    [#674] Test notification of added or removed services

--- iceoryx2-services/discovery/tests/service_monitor_tests.rs
+++ iceoryx2-services/discovery/tests/service_monitor_tests.rs
@@ -29,19 +29,15 @@ mod service_monitor {
     }
 
     #[test]
-    fn publishes_added_and_removed_services_when_configured() {
+    fn publishes_details_of_added_and_removed_services_when_configured() {
         const NUMBER_OF_SERVICES_ADDED: usize = 5;
         const NUMBER_OF_SERVICES_REMOVED: usize = 3;
 
         let iceoryx_config = generate_isolated_config();
-        let node = NodeBuilder::new()
-            .config(&iceoryx_config)
-            .create::<ipc::Service>()
-            .unwrap();
 
         // create a service monitoring service
         let service_name_string: String = INTERNAL_SERVICE_PREFIX.to_owned()
-            + "test/service_monitor/publishes_added_services_when_configured";
+            + "test/service_monitor/publishes_details_of_added_and_removed_services_when_configured";
         let monitor_config = MonitorConfig {
             service_name: service_name_string.to_string(),
             include_internal: false,
@@ -51,6 +47,11 @@ mod service_monitor {
         let mut sut = Monitor::<ipc::Service>::new(&monitor_config, &iceoryx_config);
 
         // subscribe to the monitoring service
+        let node = NodeBuilder::new()
+            .config(&iceoryx_config)
+            .create::<ipc::Service>()
+            .unwrap();
+
         let service_name = ServiceName::new(service_name_string.as_str()).unwrap();
         let service = node
             .service_builder(&service_name)
@@ -96,15 +97,57 @@ mod service_monitor {
     }
 
     #[test]
-    fn sends_notifications_when_configured() {}
-
-    #[test]
-    fn monitors_internal_services_when_configured() {
+    fn sends_events_for_added_or_removed_services_when_configured() {
         let iceoryx_config = generate_isolated_config();
+
+        // create a service monitoring service
+        let service_name_string: String = INTERNAL_SERVICE_PREFIX.to_owned()
+            + "test/service_monitor/sends_events_for_added_or_removed_services_when_configured";
+        let monitor_config = MonitorConfig {
+            service_name: service_name_string.to_string(),
+            include_internal: false,
+            publish_events: false,
+            send_notifications: true,
+        };
+        let mut sut = Monitor::<ipc::Service>::new(&monitor_config, &iceoryx_config);
+
+        // listen to the monitoring service
         let node = NodeBuilder::new()
             .config(&iceoryx_config)
             .create::<ipc::Service>()
             .unwrap();
+
+        let service_name = ServiceName::new(service_name_string.as_str()).unwrap();
+        let service = node.service_builder(&service_name).event().open().unwrap();
+        let listener = service.listener_builder().create().unwrap();
+
+        // add a service
+        let service_name = generate_name();
+        let service = node
+            .service_builder(&service_name)
+            .publish_subscribe::<u64>()
+            .create()
+            .unwrap();
+        sut.spin();
+
+        let result = listener.try_wait_one();
+        assert_that!(result, is_ok);
+        let result = result.unwrap();
+        assert_that!(result, is_some);
+
+        // remove a service
+        drop(service);
+        sut.spin();
+
+        let result = listener.try_wait_one();
+        assert_that!(result, is_ok);
+        let result = result.unwrap();
+        assert_that!(result, is_some);
+    }
+
+    #[test]
+    fn monitors_internal_services_when_configured() {
+        let iceoryx_config = generate_isolated_config();
 
         // create a service monitoring service
         let service_name_string: String = INTERNAL_SERVICE_PREFIX.to_owned()
@@ -118,6 +161,11 @@ mod service_monitor {
         let mut sut = Monitor::<ipc::Service>::new(&monitor_config, &iceoryx_config);
 
         // subscribe to the monitoring service
+        let node = NodeBuilder::new()
+            .config(&iceoryx_config)
+            .create::<ipc::Service>()
+            .unwrap();
+
         let service_name = ServiceName::new(service_name_string.as_str()).unwrap();
         let service = node
             .service_builder(&service_name)
