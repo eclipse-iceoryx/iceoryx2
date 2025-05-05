2b9fbf8d34ab3d1c25696637dc3a711ff40d0214
Jeff Ithier <jeff@ithier.dev>
2025-05-05 12:02:07 +0200

    [#707] Increase maximum length of type name string to 256 characters

--- iceoryx2/src/constants.rs
+++ iceoryx2/src/constants.rs
@@ -16,4 +16,4 @@ pub const MAX_ATTRIBUTES: usize = 16;
 pub const MAX_ATTRIBUTE_KEY_LENGTH: usize = 64;
 pub const MAX_ATTRIBUTE_VALUE_LENGTH: usize = 256;
 
-pub const MAX_TYPE_NAME_LENGTH: usize = 128;
+pub const MAX_TYPE_NAME_LENGTH: usize = 256;
