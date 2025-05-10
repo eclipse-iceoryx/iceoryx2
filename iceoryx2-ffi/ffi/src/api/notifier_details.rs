bb7934446a1442eb4f10a0dac3f8323450eec4e6
Christian Eltzschig <me@elchris.org>
2025-05-10 17:08:44 +0200

    [#685] Add notifier details c binding

--- iceoryx2-ffi/ffi/src/api/notifier_details.rs
+++ iceoryx2-ffi/ffi/src/api/notifier_details.rs
@@ -12,40 +12,40 @@
 
 #![allow(non_camel_case_types)]
 
-use iceoryx2::service::dynamic_config::event::ListenerDetails;
+use iceoryx2::service::dynamic_config::event::NotifierDetails;
 
-use super::{iox2_node_id_ptr, iox2_unique_listener_id_h, iox2_unique_listener_id_t};
+use super::{iox2_node_id_ptr, iox2_unique_notifier_id_h, iox2_unique_notifier_id_t};
 
-/// The immutable pointer to the underlying `ListenerDetails`
-pub type iox2_listener_details_ptr = *const ListenerDetails;
+/// The immutable pointer to the underlying `NotifierDetails`
+pub type iox2_notifier_details_ptr = *const NotifierDetails;
 
-/// Returns the unique port id of the listener.
+/// Returns the unique port id of the notifier.
 ///
 /// # Safety
 ///
-/// * `handle` valid pointer to the listener details
-/// * `id_struct_ptr` - Must be either a NULL pointer or a pointer to a valid [`iox2_unique_listener_id_t`].
+/// * `handle` valid pointer to the notifier details
+/// * `id_struct_ptr` - Must be either a NULL pointer or a pointer to a valid [`iox2_unique_notifier_id_t`].
 ///   If it is a NULL pointer, the storage will be allocated on the heap.
-/// * `id_handle_ptr` valid pointer to a [`iox2_unique_listener_id_h`].
+/// * `id_handle_ptr` valid pointer to a [`iox2_unique_notifier_id_h`].
 #[no_mangle]
-pub unsafe extern "C" fn iox2_listener_details_listener_id(
-    handle: iox2_listener_details_ptr,
-    id_struct_ptr: *mut iox2_unique_listener_id_t,
-    id_handle_ptr: *mut iox2_unique_listener_id_h,
+pub unsafe extern "C" fn iox2_notifier_details_notifier_id(
+    handle: iox2_notifier_details_ptr,
+    id_struct_ptr: *mut iox2_unique_notifier_id_t,
+    id_handle_ptr: *mut iox2_unique_notifier_id_h,
 ) {
     debug_assert!(!handle.is_null());
     debug_assert!(!id_handle_ptr.is_null());
 
-    fn no_op(_: *mut iox2_unique_listener_id_t) {}
-    let mut deleter: fn(*mut iox2_unique_listener_id_t) = no_op;
+    fn no_op(_: *mut iox2_unique_notifier_id_t) {}
+    let mut deleter: fn(*mut iox2_unique_notifier_id_t) = no_op;
     let mut storage_ptr = id_struct_ptr;
     if id_struct_ptr.is_null() {
-        deleter = iox2_unique_listener_id_t::dealloc;
-        storage_ptr = iox2_unique_listener_id_t::alloc();
+        deleter = iox2_unique_notifier_id_t::dealloc;
+        storage_ptr = iox2_unique_notifier_id_t::alloc();
     }
     debug_assert!(!storage_ptr.is_null());
 
-    let id = (*handle).listener_id;
+    let id = (*handle).notifier_id;
 
     (*storage_ptr).init(id, deleter);
     *id_handle_ptr = (*storage_ptr).as_handle();
@@ -55,10 +55,10 @@ pub unsafe extern "C" fn iox2_listener_details_listener_id(
 ///
 /// # Safety
 ///
-/// * `handle` valid pointer to the listener details
+/// * `handle` valid pointer to the notifier details
 #[no_mangle]
-pub unsafe extern "C" fn iox2_listener_details_node_id(
-    handle: iox2_listener_details_ptr,
+pub unsafe extern "C" fn iox2_notifier_details_node_id(
+    handle: iox2_notifier_details_ptr,
 ) -> iox2_node_id_ptr {
     debug_assert!(!handle.is_null());
 
