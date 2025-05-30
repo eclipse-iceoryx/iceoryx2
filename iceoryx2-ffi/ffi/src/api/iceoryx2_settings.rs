e11a755eb04be2cd5efec1a118406e5d83f658cf
Christian Eltzschig <me@elchris.org>
2025-05-30 19:30:06 +0200

    [#795] Remove unused iceoryx2 setting

--- iceoryx2-ffi/ffi/src/api/iceoryx2_settings.rs
+++ iceoryx2-ffi/ffi/src/api/iceoryx2_settings.rs
@@ -13,7 +13,6 @@
 pub const IOX2_ATTRIBUTE_KEY_LENGTH: usize = 64;
 pub const IOX2_ATTRIBUTE_VALUE_LENGTH: usize = 256;
 pub const IOX2_MAX_ATTRIBUTES_PER_SERVICE: usize = 16;
-pub const IOX2_MAX_VALUES_PER_ATTRIBUTE_KEY: usize = 8;
 pub const IOX2_NODE_NAME_LENGTH: usize = 128;
 pub const IOX2_SERVICE_NAME_LENGTH: usize = 255;
 pub const IOX2_SERVICE_ID_LENGTH: usize = 64;
