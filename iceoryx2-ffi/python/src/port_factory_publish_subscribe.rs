bd2bee26a9a33625e93d9960e0c0069a2b4138ca
Christian Eltzschig <me@elchris.org>
2025-06-17 02:06:57 +0200

    [#419] Implement port factory for publish subscribe

--- iceoryx2-ffi/python/src/port_factory_publish_subscribe.rs
+++ iceoryx2-ffi/python/src/port_factory_publish_subscribe.rs
@@ -10,9 +10,18 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-use iceoryx2::prelude::{ipc, local};
+use iceoryx2::prelude::{ipc, local, CallbackProgression, PortFactory};
 use iceoryx2::service::builder::{CustomHeaderMarker, CustomPayloadMarker};
 use pyo3::prelude::*;
+
+use crate::attribute_set::AttributeSet;
+use crate::error::NodeListFailure;
+use crate::node_id::NodeId;
+use crate::node_state::{
+    AliveNodeView, AliveNodeViewType, DeadNodeView, DeadNodeViewType, NodeState,
+};
+use crate::service_id::ServiceId;
+use crate::service_name::ServiceName;
 
 pub(crate) enum PortFactoryPublishSubscribeType {
     Ipc(
@@ -33,3 +42,83 @@ pub(crate) enum PortFactoryPublishSubscribeType {
 
 #[pyclass]
 pub struct PortFactoryPublishSubscribe(pub(crate) PortFactoryPublishSubscribeType);
+
+#[pymethods]
+impl PortFactoryPublishSubscribe {
+    #[getter]
+    /// Returns the `ServiceName` of the service
+    pub fn name(&self) -> ServiceName {
+        match &self.0 {
+            PortFactoryPublishSubscribeType::Ipc(v) => ServiceName(v.name().clone()),
+            PortFactoryPublishSubscribeType::Local(v) => ServiceName(v.name().clone()),
+        }
+    }
+
+    #[getter]
+    /// Returns the `ServiceId` of the `Service`
+    pub fn service_id(&self) -> ServiceId {
+        match &self.0 {
+            PortFactoryPublishSubscribeType::Ipc(v) => ServiceId(v.service_id().clone()),
+            PortFactoryPublishSubscribeType::Local(v) => ServiceId(v.service_id().clone()),
+        }
+    }
+
+    #[getter]
+    /// Returns the `AttributeSet` defined in the `Service`
+    pub fn attributes(&self) -> AttributeSet {
+        match &self.0 {
+            PortFactoryPublishSubscribeType::Ipc(v) => AttributeSet(v.attributes().clone()),
+            PortFactoryPublishSubscribeType::Local(v) => AttributeSet(v.attributes().clone()),
+        }
+    }
+
+    #[getter]
+    /// Returns a list of all `NodeState` of all the `Node`s which have opened the `Service`.
+    pub fn nodes(&self) -> PyResult<Vec<NodeState>> {
+        match &self.0 {
+            PortFactoryPublishSubscribeType::Ipc(v) => {
+                let mut ret_val = vec![];
+                v.nodes(|state| {
+                    match state {
+                        iceoryx2::prelude::NodeState::Alive(n) => {
+                            ret_val.push(NodeState::Alive(AliveNodeView(AliveNodeViewType::Ipc(n))))
+                        }
+                        iceoryx2::prelude::NodeState::Dead(n) => {
+                            ret_val.push(NodeState::Dead(DeadNodeView(DeadNodeViewType::Ipc(n))))
+                        }
+                        iceoryx2::prelude::NodeState::Inaccessible(n) => {
+                            ret_val.push(NodeState::Inaccessible(NodeId(n)))
+                        }
+                        iceoryx2::prelude::NodeState::Undefined(n) => {
+                            ret_val.push(NodeState::Undefined(NodeId(n)))
+                        }
+                    }
+                    CallbackProgression::Continue
+                })
+                .map_err(|e| NodeListFailure::new_err(format!("{:?}", e)))?;
+                Ok(ret_val)
+            }
+            PortFactoryPublishSubscribeType::Local(v) => {
+                let mut ret_val = vec![];
+                v.nodes(|state| {
+                    match state {
+                        iceoryx2::prelude::NodeState::Alive(n) => ret_val
+                            .push(NodeState::Alive(AliveNodeView(AliveNodeViewType::Local(n)))),
+                        iceoryx2::prelude::NodeState::Dead(n) => {
+                            ret_val.push(NodeState::Dead(DeadNodeView(DeadNodeViewType::Local(n))))
+                        }
+                        iceoryx2::prelude::NodeState::Inaccessible(n) => {
+                            ret_val.push(NodeState::Inaccessible(NodeId(n)))
+                        }
+                        iceoryx2::prelude::NodeState::Undefined(n) => {
+                            ret_val.push(NodeState::Undefined(NodeId(n)))
+                        }
+                    }
+                    CallbackProgression::Continue
+                })
+                .map_err(|e| NodeListFailure::new_err(format!("{:?}", e)))?;
+                Ok(ret_val)
+            }
+        }
+    }
+}
