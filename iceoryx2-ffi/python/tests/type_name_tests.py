7573dd713fce2fc62e03d3e4be739732514d6524
Christian Eltzschig <me@elchris.org>
2025-06-18 14:46:40 +0200

    [#419] Add tests for service and type name

---
+++ iceoryx2-ffi/python/tests/type_name_tests.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2025 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache Software License 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0, or the MIT license
+# which is available at https://opensource.org/licenses/MIT.
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+
+import iceoryx2_ffi_python as iox2
+import pytest
+
+
+def test_type_name_can_be_constructed() -> None:
+    sut = iox2.TypeName.new("hello world")
+    assert sut.to_string() == "hello world"
+
+
+def test_type_name_construction_fails_when_max_len_is_exceeded() -> None:
+    sut_value = "x" * (iox2.TypeName.max_len() + 1)
+    with pytest.raises(iox2.SemanticStringError):
+        sut = iox2.TypeName.new(sut_value)
