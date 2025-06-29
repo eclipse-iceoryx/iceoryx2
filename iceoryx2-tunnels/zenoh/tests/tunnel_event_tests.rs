f94678e20f9177959bbcfcfc19867b02e359e732
Jeff Ithier <jeff@ithier.dev>
2025-06-29 02:41:20 +0200

    [#845] Test discovery of Event services

--- iceoryx2-tunnels/zenoh/tests/tunnel_event_tests.rs
+++ iceoryx2-tunnels/zenoh/tests/tunnel_event_tests.rs
@@ -12,7 +12,6 @@
 
 mod testing;
 
-// TODO: test discovery of event services
 #[generic_tests::define]
 mod zenoh_tunnel_events {
 
@@ -21,10 +20,15 @@ mod zenoh_tunnel_events {
     use std::time::Duration;
 
     use iceoryx2::prelude::*;
+    use iceoryx2::service::static_config::StaticConfig;
     use iceoryx2::testing::*;
     use iceoryx2_bb_posix::unique_system_id::UniqueSystemId;
-    use iceoryx2_bb_testing::assert_that;
+    use iceoryx2_bb_testing::{assert_that, test_fail};
+    use iceoryx2_services_discovery::service_discovery::Config as DiscoveryConfig;
+    use iceoryx2_services_discovery::service_discovery::Service as DiscoveryService;
     use iceoryx2_tunnels_zenoh::*;
+
+    use zenoh::Wait;
 
     fn mock_service_name() -> ServiceName {
         ServiceName::new(&format!(
@@ -32,6 +36,243 @@ mod zenoh_tunnel_events {
             UniqueSystemId::new().unwrap().value()
         ))
         .unwrap()
+    }
+
+    #[test]
+    fn discovers_local_services_via_discovery_service<S: Service>() {
+        // ==================== SETUP ====================
+
+        // [[ COMMON ]]
+        let iox_service_name = mock_service_name();
+        let iox_config = generate_isolated_config();
+
+        // [[ DISCOVERY SERVICE ]]
+        let discovery_config = DiscoveryConfig {
+            publish_events: true,
+            ..Default::default()
+        };
+        let mut discovery_service =
+            DiscoveryService::<S>::create(&discovery_config, &iox_config).unwrap();
+
+        // [[ HOST A ]]
+        // Tunnel
+        let z_config_a = zenoh::Config::default();
+        let tunnel_config = TunnelConfig {
+            discovery_service: Some("iox2://discovery/services/".into()),
+        };
+
+        let mut tunnel = Tunnel::<S>::create(&tunnel_config, &iox_config, &z_config_a).unwrap();
+        assert_that!(tunnel.open_channels().len(), eq 0);
+
+        // Service
+        let iox_node = NodeBuilder::new()
+            .config(&iox_config)
+            .create::<S>()
+            .unwrap();
+        let iox_service = iox_node
+            .service_builder(&iox_service_name)
+            .event()
+            .open_or_create()
+            .unwrap();
+
+        // ==================== TEST =====================
+
+        // [[ DISCOVERY SERVICE ]]
+        // Discover
+        discovery_service.spin(|_| {}, |_| {}).unwrap();
+
+        // [[ HOST A ]]
+        // Respond to discovered services
+        tunnel.discover(Scope::Iceoryx).unwrap();
+        assert_that!(tunnel.open_channels().len(), eq 2);
+        assert_that!(tunnel
+            .open_channels()
+            .contains(&ChannelInfo::Notifier(String::from(iox_service.service_id().as_str()))), eq true);
+        assert_that!(tunnel
+            .open_channels()
+            .contains(&ChannelInfo::Listener(String::from(iox_service.service_id().as_str()))), eq true);
+    }
+
+    #[test]
+    fn discovers_local_services_via_tracker<S: Service>() {
+        // ==================== SETUP ====================
+
+        // [[ COMMON ]]
+        let iox_service_name = mock_service_name();
+
+        // [[ HOST A ]]
+        // Tunnel
+        let z_config = zenoh::Config::default();
+        let iox_config = generate_isolated_config();
+        let tunnel_config = TunnelConfig::default();
+        let mut tunnel = Tunnel::<S>::create(&tunnel_config, &iox_config, &z_config).unwrap();
+        assert_that!(tunnel.open_channels().len(), eq 0);
+
+        // Service
+        let iox_node = NodeBuilder::new()
+            .config(&iox_config)
+            .create::<S>()
+            .unwrap();
+        let iox_service = iox_node
+            .service_builder(&iox_service_name)
+            .event()
+            .open_or_create()
+            .unwrap();
+
+        // ==================== TEST =====================
+
+        // [[ HOST A ]]
+        // Discover
+        tunnel.discover(Scope::Iceoryx).unwrap();
+        assert_that!(tunnel.open_channels().len(), eq 2);
+        assert_that!(tunnel
+            .open_channels()
+            .contains(&ChannelInfo::Notifier(String::from(iox_service.service_id().as_str()))), eq true);
+        assert_that!(tunnel
+            .open_channels()
+            .contains(&ChannelInfo::Listener(String::from(iox_service.service_id().as_str()))), eq true);
+    }
+
+    #[test]
+    fn announces_service_details_on_zenoh<S: Service>() {
+        // ==================== SETUP ====================
+
+        // [[ COMMON ]]
+        let iox_service_name = mock_service_name();
+
+        // [[ HOST A ]]
+        // Tunnel
+        let iox_config = generate_isolated_config();
+        let z_config = zenoh::Config::default();
+        let tunnel_config = TunnelConfig::default();
+
+        let mut tunnel = Tunnel::<S>::create(&tunnel_config, &iox_config, &z_config).unwrap();
+
+        // Service
+        let iox_node = NodeBuilder::new()
+            .config(&iox_config)
+            .create::<S>()
+            .unwrap();
+        let iox_service = iox_node
+            .service_builder(&iox_service_name)
+            .event()
+            .open_or_create()
+            .unwrap();
+
+        // ==================== TEST =====================
+
+        // Discover
+        tunnel.discover(Scope::Iceoryx).unwrap();
+        assert_that!(tunnel.open_channels().len(), eq 2);
+        assert_that!(tunnel
+            .open_channels()
+            .contains(&ChannelInfo::Notifier(String::from(iox_service.service_id().as_str()))), eq true);
+        assert_that!(tunnel
+            .open_channels()
+            .contains(&ChannelInfo::Listener(String::from(iox_service.service_id().as_str()))), eq true);
+
+        // Query Zenoh for Services
+        let z_config_b = zenoh::config::Config::default();
+        let z_session_b = zenoh::open(z_config_b.clone()).wait().unwrap();
+        let z_reply_b = z_session_b
+            .get(keys::service_details(iox_service.service_id()))
+            .wait()
+            .unwrap();
+        match z_reply_b.recv_timeout(Duration::from_millis(100)) {
+            Ok(Some(reply)) => match reply.result() {
+                Ok(sample) => {
+                    let iox_static_details: StaticConfig =
+                        serde_json::from_slice(&sample.payload().to_bytes()).unwrap();
+                    assert_that!(iox_static_details.service_id(), eq iox_service.service_id());
+                    assert_that!(iox_static_details.name(), eq & iox_service_name);
+                    assert_that!(iox_static_details.event(), eq iox_service.static_config());
+                }
+                Err(e) => test_fail!("error reading reply to type details query: {}", e),
+            },
+            Ok(None) => test_fail!("no reply to type details query"),
+            Err(e) => test_fail!("error querying message type details from zenoh: {}", e),
+        }
+    }
+
+    #[test]
+    fn discovers_remote_services_via_zenoh<S: Service>() {
+        const MAX_RETRIES: usize = 25;
+        const TIME_BETWEEN_RETRIES: Duration = Duration::from_millis(250);
+
+        // ==================== SETUP ====================
+
+        // [[ COMMON ]]
+        let iox_service_name = mock_service_name();
+
+        // [[ HOST A ]]
+        // Tunnel
+        let z_config_a = zenoh::Config::default();
+        let iox_config_a = generate_isolated_config();
+        let tunnel_config_a = TunnelConfig::default();
+        let mut tunnel_a =
+            Tunnel::<S>::create(&tunnel_config_a, &iox_config_a, &z_config_a).unwrap();
+        assert_that!(tunnel_a.open_channels().len(), eq 0);
+
+        // [[ HOST B ]]
+        // Tunnel
+        let z_config_b = zenoh::Config::default();
+        let iox_config_b = generate_isolated_config();
+        let tunnel_config_b = TunnelConfig::default();
+        let mut tunnel_b =
+            Tunnel::<S>::create(&tunnel_config_b, &iox_config_b, &z_config_b).unwrap();
+        assert_that!(tunnel_b.open_channels().len(), eq 0);
+
+        // Service
+        let iox_node_b = NodeBuilder::new()
+            .config(&iox_config_b)
+            .create::<S>()
+            .unwrap();
+        let iox_service_b = iox_node_b
+            .service_builder(&iox_service_name)
+            .event()
+            .open_or_create()
+            .unwrap();
+
+        // ==================== TEST =====================
+
+        // [[ HOST A ]]
+        // Discover - nothing should be discovered
+        tunnel_a.discover(Scope::Zenoh).unwrap();
+        assert_that!(tunnel_a.open_channels().len(), eq 0);
+
+        // [[ HOST B ]]
+        // Discover - service should be announced
+        tunnel_b.discover(Scope::Iceoryx).unwrap();
+        assert_that!(tunnel_b.open_channels().len(), eq 2);
+        assert_that!(tunnel_b
+            .open_channels()
+            .contains(&ChannelInfo::Notifier(String::from(iox_service_b.service_id().as_str()))), eq true);
+        assert_that!(tunnel_b
+            .open_channels()
+            .contains(&ChannelInfo::Listener(String::from(iox_service_b.service_id().as_str()))), eq true);
+
+        // [[ HOST A ]]
+        // Discover - announced service should be discovered via Zenoh
+        retry(
+            || {
+                tunnel_a.discover(Scope::Zenoh).unwrap();
+
+                let tunneled_ports = tunnel_a.open_channels();
+                let tunneled_notifier = tunneled_ports.contains(&ChannelInfo::Notifier(
+                    String::from(iox_service_b.service_id().as_str()),
+                ));
+                let tunneled_listener = tunneled_ports.contains(&ChannelInfo::Listener(
+                    String::from(iox_service_b.service_id().as_str()),
+                ));
+
+                if tunneled_notifier && tunneled_listener {
+                    return Ok(());
+                }
+                return Err("failed to discover remote services");
+            },
+            TIME_BETWEEN_RETRIES,
+            Some(MAX_RETRIES),
+        );
     }
 
     #[test]
