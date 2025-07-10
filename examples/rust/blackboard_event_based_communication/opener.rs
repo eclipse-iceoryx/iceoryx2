57f3793bbc41688e69fae8e1426262a7c6585a23
Marika <marika@ekxide.io>
2025-07-10 16:50:25 +0200

    [#817] Extend simple reader/writer example

--- examples/rust/blackboard_event_based_communication/opener.rs
+++ examples/rust/blackboard_event_based_communication/opener.rs
@@ -10,13 +10,14 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
+// TODO: Bazel
 use core::time::Duration;
 use iceoryx2::prelude::*;
 
 const CYCLE_TIME: Duration = Duration::from_secs(1);
 
 fn main() -> Result<(), Box<dyn core::error::Error>> {
-    set_log_level_from_env_or(LogLevel::Trace);
+    set_log_level_from_env_or(LogLevel::Info);
     let node = NodeBuilder::new().create::<ipc::Service>()?;
     type KeyType = u32;
     let key = 99;
