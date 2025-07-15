b884f894148adb0352877ed912014889d42a8646
Christian Eltzschig <me@elchris.org>
2025-07-15 15:53:41 +0200

    [#419] Finalize server tests

--- iceoryx2-ffi/python/tests/server_tests.py
+++ iceoryx2-ffi/python/tests/server_tests.py
@@ -58,3 +58,91 @@ def test_unable_to_deliver_strategy_can_be_configured(
     )
 
 
+@pytest.mark.parametrize("service_type", service_types)
+def test_deleting_server_removes_it_from_the_service(
+    service_type: iox2.ServiceType,
+) -> None:
+    config = iox2.testing.generate_isolated_config()
+    node = iox2.NodeBuilder.new().config(config).create(service_type)
+    service_name = iox2.testing.generate_service_name()
+    service = (
+        node.service_builder(service_name)
+        .request_response(Payload, Payload)
+        .max_servers(1)
+        .create()
+    )
+
+    sut = service.server_builder().create()
+
+    with pytest.raises(iox2.ServerCreateError):
+        sut = service.server_builder().create()
+
+    sut.delete()
+
+    try:
+        sut = service.server_builder().create()
+    except iox2.ServerCreateError:
+        assert False
+
+
+@pytest.mark.parametrize("service_type", service_types)
+def test_deleting_response_mut_uninit_removes_it(
+    service_type: iox2.ServiceType,
+) -> None:
+    config = iox2.testing.generate_isolated_config()
+    node = iox2.NodeBuilder.new().config(config).create(service_type)
+    service_name = iox2.testing.generate_service_name()
+    service = (
+        node.service_builder(service_name)
+        .request_response(ctypes.c_uint64, ctypes.c_uint64)
+        .create()
+    )
+
+    client = service.client_builder().create()
+    sut = service.server_builder().max_loaned_responses_per_request(1).create()
+    _pending_response = client.send_copy(ctypes.c_uint64(0))
+    active_request = sut.receive()
+
+    response_uninit = active_request.loan_uninit()
+    with pytest.raises(iox2.LoanError):
+        response_uninit = active_request.loan_uninit()
+
+    response_uninit.delete()
+
+    try:
+        response_uninit = active_request.loan_uninit()
+    except iox2.LoanError:
+        assert False
+
+
+@pytest.mark.parametrize("service_type", service_types)
+def test_deleting_response_mut_removes_it(
+    service_type: iox2.ServiceType,
+) -> None:
+    config = iox2.testing.generate_isolated_config()
+    node = iox2.NodeBuilder.new().config(config).create(service_type)
+    service_name = iox2.testing.generate_service_name()
+    service = (
+        node.service_builder(service_name)
+        .request_response(ctypes.c_uint64, ctypes.c_uint64)
+        .create()
+    )
+
+    client = service.client_builder().create()
+    sut = service.server_builder().max_loaned_responses_per_request(1).create()
+    _pending_response = client.send_copy(ctypes.c_uint64(0))
+    active_request = sut.receive()
+
+    response_uninit = active_request.loan_uninit()
+    response = response_uninit.assume_init()
+    with pytest.raises(iox2.LoanError):
+        response_uninit = active_request.loan_uninit()
+
+    response.delete()
+
+    try:
+        response_uninit = active_request.loan_uninit()
+    except iox2.LoanError:
+        assert False
+
+
