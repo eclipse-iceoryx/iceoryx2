ce94aba3c1a45e752b8e5abdefc6eb49b8e949a9
Marika <marika@ekxide.io>
2025-07-17 17:40:24 +0200

    [#817] Move reader to opener, writer to creator

--- examples/rust/blackboard_event_based_communication/creator.rs
+++ examples/rust/blackboard_event_based_communication/creator.rs
@@ -19,31 +19,43 @@ fn main() -> Result<(), Box<dyn core::error::Error>> {
     set_log_level_from_env_or(LogLevel::Info);
     let node = NodeBuilder::new().create::<ipc::Service>()?;
     type KeyType = u32;
-    let interesting_key = 99;
+    const INTERESTING_KEY: u32 = 1;
 
     let service = node
         .service_builder(&"My/Funk/ServiceName".try_into()?)
         .blackboard_creator::<KeyType>()
         .add_with_default::<u64>(0)
-        .add_with_default::<u64>(interesting_key)
+        .add_with_default::<u64>(INTERESTING_KEY)
         .create()?;
 
     let event_service = node
         .service_builder(&"My/Funk/ServiceName".try_into()?)
         .event()
         .open_or_create()?;
-    let listener = event_service.listener_builder().create()?;
+    let notifier = event_service.notifier_builder().create()?;
 
-    let reader = service.reader_builder().create()?;
-    let reader_handle = reader.entry::<u64>(&interesting_key)?;
+    let writer = service.writer_builder().create()?;
 
-    // wait for entry id
-    while node.wait(Duration::ZERO).is_ok() {
-        if let Ok(Some(id)) = listener.timed_wait_one(CYCLE_TIME) {
-            if id == reader_handle.entry_id() {
-                println!("read: {}", reader_handle.get());
-            }
-        }
+    let writer_handle = writer.entry::<u64>(&0)?;
+    let entry_id = writer_handle.entry_id();
+
+    let interesting_writer_handle = writer.entry::<u64>(&INTERESTING_KEY)?;
+    let interesting_entry_id = interesting_writer_handle.entry_id();
+
+    // notify with entry id
+    let mut counter: u64 = 0;
+    while node.wait(CYCLE_TIME).is_ok() {
+        counter += 1;
+        interesting_writer_handle.update_with_copy(counter);
+        notifier.notify_with_custom_event_id(interesting_entry_id)?;
+        println!(
+            "Trigger event with entry id {}",
+            interesting_entry_id.as_value()
+        );
+
+        writer_handle.update_with_copy(2 * counter);
+        notifier.notify_with_custom_event_id(entry_id)?;
+        println!("Trigger event with entry id {}", entry_id.as_value());
     }
 
     println!("exit");
