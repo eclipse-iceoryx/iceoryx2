b2e4331b3889f174d956242a92dc9747bae12dea
Andreas Weis <github@andreas-weis.net>
2025-08-18 17:08:00 +0200

    [#939] Formatting and review fixes

--- iceoryx2-bb/cxx/include/iox2/container/optional.hpp
+++ iceoryx2-bb/cxx/include/iox2/container/optional.hpp
@@ -20,11 +20,11 @@
 #include <utility>
 
 #if IOX2_CONTAINER_CONFIG_USE_STD_OPTIONAL && IOX2_CONTAINER_CONFIG_USE_CUSTOM_OPTIONAL
-#   error Optional cannot be overriden to use both custom and std variants at the same time.
+#error Optional cannot be overriden to use both custom and std variants at the same time.
 #endif
 
 #if IOX2_CONTAINER_CONFIG_USE_STD_OPTIONAL
-#   include <optional>
+#include <optional>
 #endif
 
 namespace iox2 {
@@ -32,114 +32,124 @@ namespace container {
 
 #if IOX2_CONTAINER_CONFIG_USE_STD_OPTIONAL
 
-template<typename T>
-using Optional = std::optional <T>;
+template <typename T>
+using Optional = std::optional<T>;
 using NulloptT = std::nullopt_t;
 
 #elif defined(IOX2_CONTAINER_CONFIG_USE_CUSTOM_OPTIONAL)
 
-template<typename T>
-using Optional = IOX2_CONTAINER_CONFIG_USE_CUSTOM_OPTIONAL <T>;
+template <typename T>
+using Optional = IOX2_CONTAINER_CONFIG_USE_CUSTOM_OPTIONAL<T>;
 
 #else
 
 /// A drop-in replacement for C++17 `std::optional`.
-template<class T>
+template <class T>
 class Optional;
 
 /// A drop-in replacement for C++17 `std::nullopt_t` for use with Optional.
-struct NulloptT {};
+struct NulloptT { };
 
 
 namespace detail {
-    /// Internal union implementation for Optional.
-    /// @todo Proper handling of cond. explicit, cond. noexcept and triviality of special member functions.
-    template<typename T>
-    struct OptionalValueHolder {
-        union {
-            char null;
-            typename std::remove_cv<T>::type value;
-        };
-        bool isEmpty;
-
-        constexpr OptionalValueHolder() noexcept: null(), isEmpty(true) {}
-        constexpr OptionalValueHolder(T const& v): value(v), isEmpty(false) {}
-        constexpr OptionalValueHolder(T&& v): value(std::move(v)), isEmpty(false) {}
-        constexpr OptionalValueHolder(OptionalValueHolder const& rhs)
-        :OptionalValueHolder()
-        {
-            if (!rhs.isEmpty) { set(rhs.value); }
-        }
-        constexpr OptionalValueHolder(OptionalValueHolder&& rhs)
-        :OptionalValueHolder()
-        {
-            if (!rhs.isEmpty) { set(std::move(rhs.value)); }
-        }
-
-#       if __cplusplus >= 202002L
-        constexpr
-#       endif
-        ~OptionalValueHolder() {
-            if (!isEmpty) {
-                value.~T();
-            }
-        }
-
-        constexpr OptionalValueHolder& operator=(OptionalValueHolder const& rhs) {
-            if (this != &rhs) {
-                if (rhs.isEmpty) {
-                    reset();
-                } else {
-                    set(rhs.value);
-                }
-            }
-            return *this;
-        }
-
-        constexpr OptionalValueHolder& operator=(OptionalValueHolder&& rhs) {
-            if (this != &rhs) {
-                if (rhs.isEmpty) {
-                    reset();
-                } else {
-                    set(std::move(rhs.value));
-                }
-            }
-            return *this;
-        }
-
-        constexpr void set(T const& v) {
-            if (isEmpty) {
-                isEmpty = false;
-                new(&value) T{ v };
-            } else {
-                value = v;
-            }
-        }
-
-        constexpr void set(T&& v) {
-            if (isEmpty) {
-                isEmpty = false;
-                new(&value) T{ std::move(v) };
-            } else {
-                value = std::move(v);
-            }
-        }
-
-        constexpr void reset() {
-            if (!isEmpty) {
-                value.~T();
-                isEmpty = true;
-            }
-        }
+/// Internal union implementation for Optional.
+/// @todo Proper handling of cond. explicit, cond. noexcept and triviality of special member functions.
+template <typename T>
+struct OptionalValueHolder {
+    union {
+        char null;
+        typename std::remove_cv<T>::type value;
     };
-}
+    bool isEmpty;
 
-/// @todo Iterator and monadic APIs. 
-template<typename T>
+    constexpr OptionalValueHolder() noexcept
+        : null()
+        , isEmpty(true) {
+    }
+    constexpr OptionalValueHolder(T const& v)
+        : value(v)
+        , isEmpty(false) {
+    }
+    constexpr OptionalValueHolder(T&& v)
+        : value(std::move(v))
+        , isEmpty(false) {
+    }
+    constexpr OptionalValueHolder(OptionalValueHolder const& rhs)
+        : OptionalValueHolder() {
+        if (!rhs.isEmpty) {
+            set(rhs.value);
+        }
+    }
+    constexpr OptionalValueHolder(OptionalValueHolder&& rhs)
+        : OptionalValueHolder() {
+        if (!rhs.isEmpty) {
+            set(std::move(rhs.value));
+        }
+    }
+
+#if __cplusplus >= 202002L
+    constexpr
+#endif
+        ~OptionalValueHolder() {
+        reset();
+    }
+
+    constexpr OptionalValueHolder& operator=(OptionalValueHolder const& rhs) {
+        if (this != &rhs) {
+            if (rhs.isEmpty) {
+                reset();
+            } else {
+                set(rhs.value);
+            }
+        }
+        return *this;
+    }
+
+    constexpr OptionalValueHolder& operator=(OptionalValueHolder&& rhs) {
+        if (this != &rhs) {
+            if (rhs.isEmpty) {
+                reset();
+            } else {
+                set(std::move(rhs.value));
+            }
+        }
+        return *this;
+    }
+
+    constexpr void set(T const& v) {
+        if (isEmpty) {
+            isEmpty = false;
+            new (&value) T { v };
+        } else {
+            value = v;
+        }
+    }
+
+    constexpr void set(T&& v) {
+        if (isEmpty) {
+            isEmpty = false;
+            new (&value) T { std::move(v) };
+        } else {
+            value = std::move(v);
+        }
+    }
+
+    constexpr void reset() {
+        if (!isEmpty) {
+            value.~T();
+            isEmpty = true;
+        }
+    }
+};
+} // namespace detail
+
+/// @todo Iterator and monadic APIs.
+template <typename T>
 class Optional {
-private:
+  private:
     detail::OptionalValueHolder<T> m_value;
-public:
+
+  public:
     using value_type = T;
 
     // constructors
@@ -149,24 +159,23 @@ public:
     constexpr Optional(Optional&& rhs) = default;
 
     constexpr Optional(NulloptT) noexcept
-    :Optional()
-    {}
+        : Optional() {
+    }
 
-    template<typename U = typename std::remove_cv<T>::type,
-             typename std::enable_if<
-                    std::is_constructible<T, U>::value &&
-                    !std::is_same<typename std::decay<U>::type, Optional<T>>::value &&
-                    !std::is_same<typename std::decay<U>::type, NulloptT>::value,
-                bool>::type = true>
+    template <typename U = typename std::remove_cv<T>::type,
+              typename std::enable_if<std::is_constructible<T, U>::value
+                                          && !std::is_same<typename std::decay<U>::type, Optional<T>>::value
+                                          && !std::is_same<typename std::decay<U>::type, NulloptT>::value,
+                                      bool>::type = true>
     constexpr Optional(U&& value)
-    :m_value(std::forward<U>(value))
-    {}
+        : m_value(std::forward<U>(value)) {
+    }
 
     // destructor
 #if __cplusplus >= 202002L
     constexpr
 #endif
-    ~Optional() = default;
+        ~Optional() = default;
 
     // assignment
     constexpr Optional& operator=(NulloptT) noexcept {
@@ -194,7 +203,7 @@ public:
         }
     }
 
-    constexpr const T& operator*() const & noexcept {
+    constexpr const T& operator*() const& noexcept {
         if (m_value.isEmpty) {
             std::abort();
         }
@@ -230,7 +239,7 @@ public:
         return !m_value.isEmpty;
     }
 
-    constexpr const T& value() const & {
+    constexpr const T& value() const& {
         return **this;
     }
 
@@ -242,11 +251,12 @@ public:
         return std::move(**this);
     }
 
-    constexpr const T&& value() const && {
+    constexpr const T&& value() const&& {
         return std::move(**this);
     }
 
-    template<class U = typename std::remove_cv<T>::type> constexpr T value_or(U&& v) const & {
+    template <class U = typename std::remove_cv<T>::type>
+    constexpr T value_or(U&& v) const& {
         if (m_value.isEmpty) {
             return std::forward<U>(v);
         } else {
@@ -254,7 +264,8 @@ public:
         }
     }
 
-    template<class U = typename std::remove_cv<T>::type> constexpr T value_or(U&& v) && {
+    template <class U = typename std::remove_cv<T>::type>
+    constexpr T value_or(U&& v) && {
         if (m_value.isEmpty) {
             return std::forward<U>(v);
         } else {
@@ -269,7 +280,7 @@ public:
 };
 
 #if __cplusplus >= 201703L
-template<class T>
+template <class T>
 Optional(T) -> Optional<T>;
 #endif
 
