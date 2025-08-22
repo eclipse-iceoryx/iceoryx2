b9eb247b4352550286ebcdc66cd54d37d3659d49
Jeff Ithier <jeff@ithier.dev>
2025-08-22 17:33:22 +0200

    [#847] Wrap iovec in transparent struct

--- iceoryx2-pal/posix/src/qnx/types.rs
+++ iceoryx2-pal/posix/src/qnx/types.rs
@@ -161,74 +161,22 @@ impl MemZeroedStruct for dirent {}
 pub type msghdr = crate::internal::msghdr;
 impl MemZeroedStruct for msghdr {}
 
-impl msghdr {
-    pub fn set_name(&mut self, name: *mut ::core::ffi::c_void, namelen: socklen_t) {
-        self.msg_name = name;
-        self.msg_namelen = namelen;
-    }
-
-    pub fn set_namelen(&mut self, namelen: socklen_t) {
-        self.msg_namelen = namelen;
-    }
-
-    pub fn set_iov(&mut self, iov: *mut iovec) {
-        self.msg_iov = iov;
-    }
-
-    pub fn set_iovlen(&mut self, iovlen: ::core::ffi::c_int) {
-        self.msg_iovlen = iovlen;
-    }
-
-    pub fn set_control(&mut self, control: *mut ::core::ffi::c_void) {
-        self.msg_control = control;
-    }
-
-    pub fn set_controllen(&mut self, controllen: socklen_t) {
-        self.msg_controllen = controllen;
-    }
-
-    pub fn set_flags(&mut self, flags: ::core::ffi::c_int) {
-        self.msg_flags = flags;
-    }
-}
-
-impl Default for msghdr {
-    fn default() -> Self {
-        Self {
-            msg_name: ::core::ptr::null_mut(),
-            msg_namelen: 0,
-            msg_iov: ::core::ptr::null_mut(),
-            msg_iovlen: 0,
-            msg_control: ::core::ptr::null_mut(),
-            msg_controllen: 0,
-            msg_flags: 0,
-        }
-    }
-}
-
 pub type cmsghdr = crate::internal::cmsghdr;
 impl MemZeroedStruct for cmsghdr {}
 
-pub type iovec = crate::internal::iovec;
+#[repr(transparent)]
+pub struct iovec(crate::internal::iovec);
 impl MemZeroedStruct for iovec {}
 
 impl iovec {
     pub fn set_base(&mut self, base: *mut ::core::ffi::c_void) {
-        self.__bindgen_anon_1.iov_base = base;
+        self.0.__bindgen_anon_1.iov_base = base;
     }
     pub fn set_len(&mut self, len: usize) {
-        self.iov_len = len as _;
+        self.0.iov_len = len as _;
     }
-}
-
-impl Default for iovec {
-    fn default() -> Self {
-        Self {
-            __bindgen_anon_1: crate::internal::iovec__bindgen_ty_1 {
-                iov_base: ::core::ptr::null_mut(),
-            },
-            iov_len: 0,
-        }
+    pub fn as_mut_ptr(&mut self) -> *mut crate::internal::iovec {
+        &mut self.0
     }
 }
 
