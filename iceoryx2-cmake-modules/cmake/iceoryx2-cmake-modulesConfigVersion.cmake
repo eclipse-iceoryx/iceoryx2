295d4eb1f750e12d37d0c47eb2ba67d7701ecb7c
Mathias Kraus <elboberido@m-hias.de>
2025-09-12 20:14:04 +0200

    [#77] Update version number to v0.7.0

--- iceoryx2-cmake-modules/cmake/iceoryx2-cmake-modulesConfigVersion.cmake
+++ iceoryx2-cmake-modules/cmake/iceoryx2-cmake-modulesConfigVersion.cmake
@@ -14,7 +14,7 @@
 ########## dummyConfig.cmake to be able to use find_package with a specific version with the source tree ##########
 #
 
-set(IOX2_VERSION 0.6.1)
+set(IOX2_VERSION 0.7.0)
 set(PACKAGE_VERSION ${IOX2_VERSION})
 
 if(PACKAGE_VERSION VERSION_LESS PACKAGE_FIND_VERSION)
