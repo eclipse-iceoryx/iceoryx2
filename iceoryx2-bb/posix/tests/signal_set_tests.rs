051cc8632b7d7a198747135e395f226860ef319e
Christian Eltzschig <me@elchris.org>
2025-09-23 01:11:24 +0200

    [#961] Add signal set tests

---
+++ iceoryx2-bb/posix/tests/signal_set_tests.rs
@@ -0,0 +1,64 @@
+// Copyright (c) 2025 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache Software License 2.0 which is available at
+// https://www.apache.org/licenses/LICENSE-2.0, or the MIT license
+// which is available at https://opensource.org/licenses/MIT.
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+use enum_iterator::all;
+use iceoryx2_bb_posix::{signal::Signal, signal_set::SignalSet};
+use iceoryx2_bb_testing::assert_that;
+
+#[test]
+fn new_empty_signal_set_does_not_contain_a_signal() {
+    let sut = SignalSet::new_empty();
+
+    for signal in all::<Signal>().collect::<Vec<Signal>>() {
+        assert_that!(sut.contains(signal), eq false);
+    }
+}
+
+#[test]
+fn new_filled_signal_set_does_contain_all_signals() {
+    let sut = SignalSet::new_filled();
+
+    for signal in all::<Signal>().collect::<Vec<Signal>>() {
+        assert_that!(sut.contains(signal), eq true);
+    }
+}
+
+#[test]
+fn adding_new_signals_works() {
+    let mut sut = SignalSet::new_empty();
+
+    for signal in all::<Signal>().collect::<Vec<Signal>>() {
+        assert_that!(sut.contains(signal), eq false);
+        sut.add(signal);
+        assert_that!(sut.contains(signal), eq true);
+    }
+}
+
+#[test]
+fn removing_signals_works() {
+    let mut sut = SignalSet::new_filled();
+
+    for signal in all::<Signal>().collect::<Vec<Signal>>() {
+        assert_that!(sut.contains(signal), eq true);
+        sut.remove(signal);
+        assert_that!(sut.contains(signal), eq false);
+    }
+}
+
+#[test]
+fn create_from_pending_signals_works() {
+    let sut = SignalSet::from_pending();
+
+    for signal in all::<Signal>().collect::<Vec<Signal>>() {
+        assert_that!(sut.contains(signal), eq false);
+    }
+}
