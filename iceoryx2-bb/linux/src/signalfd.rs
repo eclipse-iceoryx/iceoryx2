604a86281e30ac8325e40ee1f0dc90f5c471c81b
Christian Eltzschig <me@elchris.org>
2025-09-23 13:34:04 +0200

    [#961] Document signal fd

--- iceoryx2-bb/linux/src/signalfd.rs
+++ iceoryx2-bb/linux/src/signalfd.rs
@@ -10,6 +10,35 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
+//! The [`SignalFd`] is a safe abstraction over the linux signal fd api. It
+//! allows users to receive a signal via a file descriptor that can be attached
+//! to a
+//! [`FileDescriptorSet`](iceoryx2_bb_posix::file_descriptor_set::FileDescriptorSet)
+//! or via [`Epoll`](crate::epoll::Epoll).
+//!
+//! # Example
+//!
+//! ```
+//! use iceoryx2_bb_linux::signalfd::SignalFdBuilder;
+//! use iceoryx2_bb_posix::signal_set::FetchableSignalSet;
+//! use iceoryx2_bb_posix::signal::FetchableSignal;
+//!
+//! # fn main() -> Result<(), Box<dyn core::error::Error>> {
+//!
+//! let mut registered_signals = FetchableSignalSet::new_empty();
+//! registered_signals.add(FetchableSignal::UserDefined1);
+//!
+//! let signal_fd = SignalFdBuilder::new(registered_signals).create_non_blocking()?;
+//!
+//! match signal_fd.try_read()? {
+//!     Some(signal) => println!("signal was raised: {signal:?}"),
+//!     None => println!("no signal was raised")
+//! }
+//!
+//! # Ok(())
+//! # }
+//! ```
+
 use std::fmt::Debug;
 
 use iceoryx2_bb_log::{fail, fatal_panic};
@@ -20,25 +49,55 @@ use iceoryx2_bb_posix::{
 use iceoryx2_pal_os_api::linux;
 use iceoryx2_pal_posix::posix::{self, MemZeroedStruct};
 
+/// Error emitted when creating a new [`SignalFd`].
 #[derive(Debug, Clone, Copy, Eq, Hash, PartialEq)]
 pub enum SignalFdCreationError {
+    /// The process wide file handle limit is reached
     PerProcessFileHandleLimitReached,
+    /// The system wide file handle limit is reached
     SystemWideFileHandleLimitReached,
+    /// Insufficient memory available
     InsufficientMemory,
+    /// The underlying inode device could not be mounted
     UnableToMountInodeDevice,
+    /// An error that was not documented in the POSIX API was reported
     UnknownError(i32),
 }
 
+impl core::fmt::Display for SignalFdCreationError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "SignalFdCreationError::{self:?}")
+    }
+}
+
+impl core::error::Error for SignalFdCreationError {}
+
+/// Error emitted from [`BlockingSignalFd::blocking_read()`] or [`SignalFd::try_read()`].
 #[derive(Debug, Clone, Copy, Eq, Hash, PartialEq)]
 pub enum SignalFdReadError {
+    /// The amount of bytes read were less than the size of the internal siginfo struct
     SystemBreaksReadContract,
+    /// An interrupt signal was raised
     Interrupt,
+    /// An input/output error occurred
     IOerror,
+    /// Insufficient resources available
     InsufficientResources,
+    /// Insufficient memory available
     InsufficientMemory,
+    /// An error that was not documented in the POSIX API was reported
     UnknownError(i32),
 }
 
+impl core::fmt::Display for SignalFdReadError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "SignalFdReadError::{self:?}")
+    }
+}
+
+impl core::error::Error for SignalFdReadError {}
+
+/// The builder that creates a [`SignalFd`] or a [`BlockingSignalFd`].
 #[derive(Debug)]
 pub struct SignalFdBuilder {
     signal_set: FetchableSignalSet,
@@ -46,6 +105,8 @@ pub struct SignalFdBuilder {
 }
 
 impl SignalFdBuilder {
+    /// Creates a new builder and the [`FetchableSignalSet`] defines which [`FetchableSignal`]s
+    /// the [`SignalFd`] shall receive.
     pub fn new(signal_set: FetchableSignalSet) -> Self {
         Self {
             signal_set,
@@ -53,17 +114,21 @@ impl SignalFdBuilder {
         }
     }
 
+    /// Defines if the underlying [`FileDescriptor`] shall be closed when the
+    /// [`Process`](iceoryx2_bb_posix::process::Process) is forked.
     pub fn set_close_on_exec(mut self) -> Self {
         self.close_on_exec = true;
         self
     }
 
+    /// Create the non-blocking version of the [`SignalFd`].
     pub fn create_non_blocking(self) -> Result<SignalFd, SignalFdCreationError> {
         Ok(SignalFd {
             file_descriptor: self.create(true)?,
         })
     }
 
+    /// Create the blocking version [`BlockingSignalFd`]
     pub fn create_blocking(self) -> Result<BlockingSignalFd, SignalFdCreationError> {
         Ok(BlockingSignalFd {
             file_descriptor: self.create(false)?,
@@ -123,24 +188,41 @@ impl SignalFdBuilder {
     }
 }
 
+/// Contains all details about the [`FetchableSignal`] that was raised.
 pub struct SignalInfo {
     signal_info: linux::signalfd_siginfo,
 }
 
+impl Debug for SignalInfo {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "SignalInfo {{ signal: {:?}, origin_pid: {}, origin_uid: {} }}",
+            self.signal(),
+            self.origin_pid(),
+            self.origin_uid()
+        )
+    }
+}
+
 impl SignalInfo {
+    /// Returns the [`FetchableSignal`] that was received.
     pub fn signal(&self) -> FetchableSignal {
         (self.signal_info.ssi_signo as i32).into()
     }
 
+    /// Returns the [`ProcessId`] of the origin that send the signal.
     pub fn origin_pid(&self) -> ProcessId {
         ProcessId::new(self.signal_info.ssi_pid as _)
     }
 
+    /// Returns the [`Uid`] of the origin that send the signal.
     pub fn origin_uid(&self) -> Uid {
         Uid::new_from_native(self.signal_info.ssi_uid)
     }
 }
 
+/// Non-blocking version of a signalfd
 #[derive(Debug)]
 pub struct SignalFd {
     file_descriptor: FileDescriptor,
@@ -203,17 +285,22 @@ fn read_from_fd<T: Debug>(
 }
 
 impl SignalFd {
+    /// Tries to read the raised [`FetchableSignal`]. If no signal was raised it returns
+    /// [`None`].
     pub fn try_read(&self) -> Result<Option<SignalInfo>, SignalFdReadError> {
         read_from_fd(self, &self.file_descriptor)
     }
 }
 
+/// Blocking version of the signal fd
 #[derive(Debug)]
 pub struct BlockingSignalFd {
     file_descriptor: FileDescriptor,
 }
 
 impl BlockingSignalFd {
+    /// Blocks until either the raised [`FetchableSignal`] was received or an error was
+    /// reported. It might have spurious wake ups.
     pub fn blocking_read(&self) -> Result<Option<SignalInfo>, SignalFdReadError> {
         read_from_fd(self, &self.file_descriptor)
     }
