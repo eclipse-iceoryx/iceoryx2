db59b163ed201c1656384e6382e99a27f760bc0b
Ziad Mostafa <mohammedziad.badr@gmail.com>
2025-10-27 21:42:22 +0100

    [eclipse-iceoryx#957] Fix linker issue, and small name typo
    Signed-off-by: Ziad Mostafa <mohammedziad.badr@gmail.com>

--- iceoryx2-cmake-modules/modules/Iceoryx2Sanitizer.cmake
+++ iceoryx2-cmake-modules/modules/Iceoryx2Sanitizer.cmake
@@ -11,18 +11,18 @@
 # SPDX-License-Identifier: Apache-2.0 OR MIT
 
 # Parse SANITIZERS string and set appropriate flags
-set(ICEORYX2_SANITZER_FLAGS "" CACHE INTERNAL "")
+set(ICEORYX2_SANITIZER_FLAGS "" CACHE INTERNAL "")
 
 if(NOT DEFINED SANITIZERS OR SANITIZERS STREQUAL "")
     # No sanitizers enabled - empty string is the default
 elseif(SANITIZERS STREQUAL "address")
-    set(ICEORYX2_SANITZER_FLAGS -fsanitize=address CACHE INTERNAL "")
+    set(ICEORYX2_SANITIZER_FLAGS -fsanitize=address CACHE INTERNAL "")
 elseif(SANITIZERS STREQUAL "ub")
-    set(ICEORYX2_SANITZER_FLAGS -fsanitize=undefined CACHE INTERNAL "")
+    set(ICEORYX2_SANITIZER_FLAGS -fsanitize=undefined CACHE INTERNAL "")
 elseif(SANITIZERS STREQUAL "address;ub")
-    set(ICEORYX2_SANITZER_FLAGS -fsanitize=address -fsanitize=undefined CACHE INTERNAL "")
+    set(ICEORYX2_SANITIZER_FLAGS -fsanitize=address -fsanitize=undefined CACHE INTERNAL "")
 elseif(SANITIZERS STREQUAL "thread")
-    set(ICEORYX2_SANITZER_FLAGS -fsanitize=thread CACHE INTERNAL "")
+    set(ICEORYX2_SANITIZER_FLAGS -fsanitize=thread CACHE INTERNAL "")
 else()
     message(FATAL_ERROR "Invalid SANITIZERS value: '${SANITIZERS}'. Valid options are: 'address', 'ub', 'address;ub', 'thread', or empty string (disabled)")
 endif()
