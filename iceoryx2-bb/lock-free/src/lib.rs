a6a4415f3e89e065a95d0981f9cbd663527bfdbc
Jeff Ithier <jeff@ithier.dev>
2025-11-14 11:46:38 +0100

    [#865] Enable std when testing iceoryx2-bb-lock-free

--- iceoryx2-bb/lock-free/src/lib.rs
+++ iceoryx2-bb/lock-free/src/lib.rs
@@ -10,7 +10,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-#![no_std]
+#![cfg_attr(not(test), no_std)]
 #![warn(clippy::alloc_instead_of_core)]
 #![warn(clippy::std_instead_of_alloc)]
 #![warn(clippy::std_instead_of_core)]
