7f5903137f2e2a68d451f20dc718129d9d1406f1
Mathias Kraus <elboberido@m-hias.de>
2025-11-30 16:59:50 +0100

    [#301] Align copyright header of new files with iceoryx2 schema

--- iceoryx2-bb/cxx/include/iox2/legacy/assertions.hpp
+++ iceoryx2-bb/cxx/include/iox2/legacy/assertions.hpp
@@ -1,16 +1,14 @@
 // Copyright (c) 2023 by Apex.AI Inc. All rights reserved.
 // Copyright (c) 2024 by ekxide IO GmbH. All rights reserved.
+// Copyright (c) 2025 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
 //
 // This program and the accompanying materials are made available under the
 // terms of the Apache Software License 2.0 which is available at
 // https://www.apache.org/licenses/LICENSE-2.0, or the MIT license
 // which is available at https://opensource.org/licenses/MIT.
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
