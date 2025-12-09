bf906d61bb4f8092a55f3764f3f31eec033e39b9
Mathias Kraus <elboberido@m-hias.de>
2025-12-09 01:41:44 +0100

    [#1234] Use new 'Optional' for 'Listener'

--- iceoryx2-cxx/tests/src/service_event_tests.cpp
+++ iceoryx2-cxx/tests/src/service_event_tests.cpp
@@ -499,8 +499,8 @@ TYPED_TEST(ServiceEventTest, deadline_can_be_set) {
 
     ASSERT_THAT(service_create.static_config().deadline(), Eq(iox2::container::Optional<Duration>(DEADLINE)));
     ASSERT_THAT(service_open.static_config().deadline(), Eq(iox2::container::Optional<Duration>(DEADLINE)));
-    ASSERT_THAT(listener_create.deadline(), Eq(iox2::legacy::optional<Duration>(DEADLINE)));
-    ASSERT_THAT(listener_open.deadline(), Eq(iox2::legacy::optional<Duration>(DEADLINE)));
+    ASSERT_THAT(listener_create.deadline(), Eq(iox2::container::Optional<Duration>(DEADLINE)));
+    ASSERT_THAT(listener_open.deadline(), Eq(iox2::container::Optional<Duration>(DEADLINE)));
     ASSERT_THAT(notifier_create.deadline(), Eq(iox2::container::Optional<Duration>(DEADLINE)));
     ASSERT_THAT(notifier_open.deadline(), Eq(iox2::container::Optional<Duration>(DEADLINE)));
 }
@@ -524,8 +524,8 @@ TYPED_TEST(ServiceEventTest, deadline_can_be_disabled) {
 
     ASSERT_THAT(service_create.static_config().deadline(), Eq(iox2::container::nullopt));
     ASSERT_THAT(service_open.static_config().deadline(), Eq(iox2::container::nullopt));
-    ASSERT_THAT(listener_create.deadline(), Eq(iox2::legacy::nullopt));
-    ASSERT_THAT(listener_open.deadline(), Eq(iox2::legacy::nullopt));
+    ASSERT_THAT(listener_create.deadline(), Eq(iox2::container::nullopt));
+    ASSERT_THAT(listener_open.deadline(), Eq(iox2::container::nullopt));
     ASSERT_THAT(notifier_create.deadline(), Eq(iox2::container::nullopt));
     ASSERT_THAT(notifier_open.deadline(), Eq(iox2::container::nullopt));
 }
