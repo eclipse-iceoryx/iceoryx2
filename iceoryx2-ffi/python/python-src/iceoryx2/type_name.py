adbcb7852054a872fe1c93411898348df8888dd3
Christian Eltzschig <me@elchris.org>
2026-01-30 09:56:08 +0100

    [#1333] Fix python linting

--- iceoryx2-ffi/python/python-src/iceoryx2/type_name.py
+++ iceoryx2-ffi/python/python-src/iceoryx2/type_name.py
@@ -18,41 +18,56 @@ from typing import Any, Type, TypeVar
 T = TypeVar("T")
 
 
+def get_unsigned_int_type_name(t: Type[T]) -> Any:
+    """Generates a human readable type name from a given unsigned integer type."""
+    if ctypes.sizeof(t) == 1:
+        return "u8"
+    if ctypes.sizeof(t) == 2:
+        return "u16"
+    if ctypes.sizeof(t) == 4:
+        return "u32"
+    if ctypes.sizeof(t) == 8:
+        return "u64"
+
+    return t.__name__
+
+
+def get_signed_int_type_name(t: Type[T]) -> Any:
+    """Generates a human readable type name from a given signed integer type."""
+    if ctypes.sizeof(t) == 1:
+        return "i8"
+    if ctypes.sizeof(t) == 2:
+        return "i16"
+    if ctypes.sizeof(t) == 4:
+        return "i32"
+    if ctypes.sizeof(t) == 8:
+        return "i64"
+
+    return t.__name__
+
+
+def get_float_type_name(t: Type[T]) -> Any:
+    """Generates a human readable type name from a given float type."""
+    if ctypes.sizeof(t) == 4:
+        return "f32"
+    if ctypes.sizeof(t) == 8:
+        return "f64"
+
+    return t.__name__
+
+
 def get_type_name(t: Type[T]) -> Any:
     """Generates a human readable type name from a given type."""
     if hasattr(t, "type_name"):
         return t.type_name()
 
-    if t.__name__ == "c_ubyte" or t.__name__ == "c_ushort" or t.__name__ == "c_uint" or t.__name__ == "c_ulong":
-        if ctypes.sizeof(t) == 1:
-            return "u8"
-        elif ctypes.sizeof(t) == 2:
-            return "u16"
-        elif ctypes.sizeof(t) == 4:
-            return "u32"
-        elif ctypes.sizeof(t) == 8:
-            return "u64"
-        else:
-            return t.__name__
-    elif t.__name__ == "c_byte" or t.__name__ == "c_short" or t.__name__ == "c_int" or t.__name__ == "c_long":
-        if ctypes.sizeof(t) == 1:
-            return "i8"
-        elif ctypes.sizeof(t) == 2:
-            return "i16"
-        elif ctypes.sizeof(t) == 4:
-            return "i32"
-        elif ctypes.sizeof(t) == 8:
-            return "i64"
-        else:
-            return t.__name__
-    elif t.__name__ == "c_float" or t.__name__ == "c_double":
-        if ctypes.sizeof(t) == 4:
-            return "f32"
-        elif ctypes.sizeof(t) == 8:
-            return "f64"
-        else:
-            return t.__name__
-    elif t.__name__ == "c_bool":
+    if t.__name__ in ("c_ubyte", "c_ushort", "c_uint", "c_ulong"):
+        return get_unsigned_int_type_name(t)
+    if t.__name__ in ("c_byte", "c_short", "c_int", "c_long"):
+        return get_signed_int_type_name(t)
+    if t.__name__ in ("c_float", "c_double"):
+        return get_float_type_name(t)
+    if t.__name__ == "c_bool":
         return "bool"
-    else:
-        return t.__name__
+
+    return t.__name__
