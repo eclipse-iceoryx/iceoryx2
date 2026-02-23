e4de3c0a184ee473a4001d0e42d6b88fb64e1806
Jeff Ithier <jeff@ithier.dev>
2026-02-23 14:57:25 +0100

    [#1300] Add no_std tests for iceoryx2-bb-elementary

---
+++ iceoryx2-bb/elementary/tests-nostd/src/relocatable_ptr_tests.rs
@@ -0,0 +1,21 @@
+// Copyright (c) 2026 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache Software License 2.0 which is available at
+// https://www.apache.org/licenses/LICENSE-2.0, or the MIT license
+// which is available at https://opensource.org/licenses/MIT.
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+#![allow(clippy::disallowed_types)]
+
+use iceoryx2_bb_elementary_tests_common::relocatable_ptr_tests;
+use iceoryx2_bb_testing_nostd_macros::inventory_test;
+
+#[inventory_test]
+pub fn relocatable_pointer_works() {
+    relocatable_ptr_tests::relocatable_pointer_works();
+}
