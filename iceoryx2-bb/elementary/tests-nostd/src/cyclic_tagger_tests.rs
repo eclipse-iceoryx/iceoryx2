b7ee143aa69adec5d6ee7b1b9242bb4343943043
Jeff Ithier <jeff@ithier.dev>
2026-02-23 14:57:25 +0100

    [#1300] Add no_std tests for iceoryx2-bb-elementary

---
+++ iceoryx2-bb/elementary/tests-nostd/src/cyclic_tagger_tests.rs
@@ -0,0 +1,31 @@
+// Copyright (c) 2026 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache Software License 2.0 which is available at
+// https://www.apache.org/licenses/LICENSE-2.0, or the MIT license
+// which is available at https://opensource.org/licenses/MIT.
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+#![allow(clippy::disallowed_types)]
+
+use iceoryx2_bb_elementary_tests_common::cyclic_tagger_tests;
+use iceoryx2_bb_testing_nostd_macros::inventory_test;
+
+#[inventory_test]
+pub fn create_tag_works() {
+    cyclic_tagger_tests::create_tag_works();
+}
+
+#[inventory_test]
+pub fn create_untagged_tag_works() {
+    cyclic_tagger_tests::create_untagged_tag_works();
+}
+
+#[inventory_test]
+pub fn tagging_after_new_cyclic_works() {
+    cyclic_tagger_tests::tagging_after_new_cyclic_works();
+}
