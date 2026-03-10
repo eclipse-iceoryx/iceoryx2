5ba9385fbe33677a3c83c931ec9b98994c878a36
Jeff Ithier <jeff@ithier.dev>
2026-03-10 15:06:44 +0100

    [#1300] Make iceoryx2-bb-trait-tests tests-common a library

--- iceoryx2-bb/trait-tests/tests-common/src/relocatable_container_tests.rs
+++ iceoryx2-bb/trait-tests/tests-common/src/relocatable_container_tests.rs
@@ -10,125 +10,72 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-extern crate iceoryx2_bb_loggers;
+use alloc::boxed::Box;
 
-#[generic_tests::define]
-mod relocatable_container {
-    use core::ptr::NonNull;
-    use iceoryx2_bb_container::{queue::RelocatableQueue, vector::relocatable_vec::RelocatableVec};
-    use iceoryx2_bb_elementary_traits::relocatable_container::RelocatableContainer;
-    use iceoryx2_bb_lock_free::{
-        mpmc::{container::Container, unique_index_set::UniqueIndexSet},
-        spsc::{
-            index_queue::RelocatableIndexQueue,
-            safely_overflowing_index_queue::RelocatableSafelyOverflowingIndexQueue,
-        },
-    };
-    use iceoryx2_bb_memory::bump_allocator::BumpAllocator;
-    use iceoryx2_bb_testing::assert_that;
-    use iceoryx2_cal::zero_copy_connection::used_chunk_list::RelocatableUsedChunkList;
+use core::ptr::NonNull;
+use iceoryx2_bb_elementary_traits::relocatable_container::RelocatableContainer;
+use iceoryx2_bb_memory::bump_allocator::BumpAllocator;
+use iceoryx2_bb_testing::assert_that;
+use iceoryx2_bb_testing_nostd_macros::requires_std;
 
-    const MEMORY_SIZE: usize = 1024 * 128;
+const MEMORY_SIZE: usize = 1024 * 128;
 
-    fn memory() -> Box<[u8; MEMORY_SIZE]> {
-        Box::new([0u8; MEMORY_SIZE])
-    }
+fn memory() -> Box<[u8; MEMORY_SIZE]> {
+    Box::new([0u8; MEMORY_SIZE])
+}
 
-    fn allocator(memory: &mut [u8]) -> BumpAllocator {
-        BumpAllocator::new(
-            NonNull::new(memory.as_mut_ptr() as *mut u8).unwrap(),
-            memory.len(),
-        )
-    }
+fn allocator(memory: &mut [u8]) -> BumpAllocator {
+    BumpAllocator::new(
+        NonNull::new(memory.as_mut_ptr() as *mut u8).unwrap(),
+        memory.len(),
+    )
+}
 
-    #[test]
-    fn init_acquires_less_or_equal_the_required_size_of_bytes<T: RelocatableContainer>() {
-        const MAX_CAPACITY: usize = 128;
+pub fn init_acquires_less_or_equal_the_required_size_of_bytes<T: RelocatableContainer>() {
+    const MAX_CAPACITY: usize = 128;
 
-        for capacity in 1..MAX_CAPACITY {
-            let mut memory = memory();
-            let allocator = allocator(&mut *memory);
-
-            let mut sut = unsafe { T::new_uninit(capacity) };
-            let require_memory_size = T::memory_size(capacity);
-
-            assert_that!(unsafe { sut.init(&allocator) }, is_ok);
-            assert_that!(allocator.used_space(), le require_memory_size);
-        }
-    }
-
-    #[test]
-    fn init_acquires_less_or_equal_the_required_size_of_bytes_multiple_allocations<
-        T: RelocatableContainer,
-    >() {
-        const MAX_CAPACITY: usize = 18;
-
+    for capacity in 1..MAX_CAPACITY {
         let mut memory = memory();
         let allocator = allocator(&mut *memory);
 
-        let mut current_size = 0;
-        for capacity in 1..MAX_CAPACITY {
-            let mut sut = unsafe { T::new_uninit(capacity) };
-            let require_memory_size = T::memory_size(capacity);
-
-            assert_that!(unsafe { sut.init(&allocator) }, is_ok);
-            assert_that!(allocator.used_space(), le current_size + require_memory_size);
-            current_size = allocator.used_space();
-        }
-    }
-
-    #[test]
-    #[should_panic]
-    fn init_twice_causes_panic<T: RelocatableContainer>() {
-        const MAX_CAPACITY: usize = 18;
-
-        let mut memory = memory();
-        let allocator = allocator(&mut *memory);
-
-        let mut sut = unsafe { T::new_uninit(MAX_CAPACITY) };
+        let mut sut = unsafe { T::new_uninit(capacity) };
+        let require_memory_size = T::memory_size(capacity);
 
         assert_that!(unsafe { sut.init(&allocator) }, is_ok);
-
-        //panics
-        assert_that!(unsafe { sut.init(&allocator) }, is_ok);
+        assert_that!(allocator.used_space(), le require_memory_size);
     }
+}
 
-    #[instantiate_tests(<RelocatableVec<u64>>)]
-    mod vec_u64 {}
+pub fn init_acquires_less_or_equal_the_required_size_of_bytes_multiple_allocations<
+    T: RelocatableContainer,
+>() {
+    const MAX_CAPACITY: usize = 18;
 
-    #[instantiate_tests(<RelocatableVec<u128>>)]
-    mod vec_u128 {}
+    let mut memory = memory();
+    let allocator = allocator(&mut *memory);
 
-    #[instantiate_tests(<RelocatableVec<[u8; 123]>>)]
-    mod vec_special {}
+    let mut current_size = 0;
+    for capacity in 1..MAX_CAPACITY {
+        let mut sut = unsafe { T::new_uninit(capacity) };
+        let require_memory_size = T::memory_size(capacity);
 
-    #[instantiate_tests(<RelocatableQueue<u64>>)]
-    mod queue_u64 {}
+        assert_that!(unsafe { sut.init(&allocator) }, is_ok);
+        assert_that!(allocator.used_space(), le current_size + require_memory_size);
+        current_size = allocator.used_space();
+    }
+}
 
-    #[instantiate_tests(<RelocatableQueue<u128>>)]
-    mod queue_u128 {}
+#[requires_std("panics")]
+pub fn init_twice_causes_panic<T: RelocatableContainer>() {
+    const MAX_CAPACITY: usize = 18;
 
-    #[instantiate_tests(<RelocatableQueue<[u8; 123]>>)]
-    mod queue_special {}
+    let mut memory = memory();
+    let allocator = allocator(&mut *memory);
 
-    #[instantiate_tests(<Container<u64>>)]
-    mod container_u64 {}
+    let mut sut = unsafe { T::new_uninit(MAX_CAPACITY) };
 
-    #[instantiate_tests(<Container<u128>>)]
-    mod container_u128 {}
+    assert_that!(unsafe { sut.init(&allocator) }, is_ok);
 
-    #[instantiate_tests(<Container<[u8; 123]>>)]
-    mod container_special {}
-
-    #[instantiate_tests(<UniqueIndexSet>)]
-    mod unique_index_set {}
-
-    #[instantiate_tests(<RelocatableIndexQueue>)]
-    mod index_queue {}
-
-    #[instantiate_tests(<RelocatableSafelyOverflowingIndexQueue>)]
-    mod safely_overflowing_index_queue {}
-
-    #[instantiate_tests(<RelocatableUsedChunkList>)]
-    mod used_chunk_list {}
+    //panics
+    assert_that!(unsafe { sut.init(&allocator) }, is_ok);
 }
--- iceoryx2-bb/trait-tests/tests-common/src/semantic_string_tests.rs
+++ iceoryx2-bb/trait-tests/tests-common/src/semantic_string_tests.rs
@@ -12,277 +12,226 @@
 
 extern crate iceoryx2_bb_loggers;
 
+use alloc::format;
+use alloc::vec;
+
 use iceoryx2_bb_container::semantic_string::*;
-use iceoryx2_bb_system_types::base64url::*;
-use iceoryx2_bb_system_types::file_name::*;
-use iceoryx2_bb_system_types::file_path::*;
-use iceoryx2_bb_system_types::group_name::*;
-use iceoryx2_bb_system_types::path::*;
-use iceoryx2_bb_system_types::user_name::*;
 use iceoryx2_bb_testing::assert_that;
 
-#[test]
-fn display_error_enum_works() {
+pub fn display_error_enum_works() {
     assert_that!(format!("{}", SemanticStringError::InvalidContent), eq "SemanticStringError::InvalidContent");
     assert_that!(format!("{}", SemanticStringError::ExceedsMaximumLength), eq "SemanticStringError::ExceedsMaximumLength");
 }
 
-#[generic_tests::define]
-mod semantic_string {
+pub fn new_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
+    let sut = Sut::new(b"hello-txt");
+    assert_that!(sut, is_ok);
+    let sut = sut.unwrap();
 
-    use super::*;
+    assert_that!(sut, len 9);
+    assert_that!(sut.as_bytes(), eq b"hello-txt");
+}
 
-    #[test]
-    fn new_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
-        let sut = Sut::new(b"hello-txt");
-        assert_that!(sut, is_ok);
-        let sut = sut.unwrap();
+pub fn new_name_with_illegal_char_is_illegal<
+    const CAPACITY: usize,
+    Sut: SemanticString<CAPACITY>,
+>() {
+    let sut = Sut::new(b"hello \0.txt");
+    assert_that!(sut, is_err);
+}
 
-        assert_that!(sut, len 9);
-        assert_that!(sut.as_bytes(), eq b"hello-txt");
+pub fn try_from_legal_str_succeeds<
+    const CAPACITY: usize,
+    Sut: SemanticString<CAPACITY> + TryFrom<&'static str>,
+>() {
+    let sut = Sut::try_from("woohoo-md");
+    assert_that!(sut, is_ok);
+    let sut = unsafe { sut.unwrap_unchecked() };
+
+    assert_that!(sut, len 9);
+    assert_that!(sut.as_bytes(), eq b"woohoo-md");
+}
+
+pub fn try_from_illegal_str_fails<
+    const CAPACITY: usize,
+    Sut: SemanticString<CAPACITY> + TryFrom<&'static str>,
+>() {
+    let sut = Sut::try_from("oh zero \0.txt");
+    assert_that!(sut, is_err);
+}
+
+pub fn insert_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
+    let mut sut = Sut::new(b"hello").unwrap();
+    assert_that!(sut.insert(1, b't'), is_ok);
+
+    assert_that!(sut, len 6);
+    assert_that!(sut.as_bytes(), eq b"htello");
+}
+
+pub fn insert_illegal_character_fails<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
+    let mut sut = Sut::new(b"hello").unwrap();
+    assert_that!(sut.insert(1, b'*'), is_err);
+
+    assert_that!(sut, len 5);
+    assert_that!(sut.as_bytes(), eq b"hello");
+}
+
+pub fn insert_bytes_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
+    let mut sut = Sut::new(b"wld").unwrap();
+    assert_that!(sut.insert_bytes(1, b"or"), is_ok);
+
+    assert_that!(sut, len 5);
+    assert_that!(sut.as_bytes(), eq b"world");
+}
+
+pub fn insert_bytes_with_illegal_character_fails<
+    const CAPACITY: usize,
+    Sut: SemanticString<CAPACITY>,
+>() {
+    let mut sut = Sut::new(b"wld").unwrap();
+    assert_that!(sut.insert_bytes(1, b"o\0r"), is_err);
+
+    assert_that!(sut, len 3);
+    assert_that!(sut.as_bytes(), eq b"wld");
+}
+
+pub fn pop_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
+    let mut sut = Sut::new(b"fuu-blaa-fuu").unwrap();
+    let result = sut.pop();
+    assert_that!(result.unwrap(), eq Some(b'u'));
+
+    assert_that!(sut, len 11);
+    assert_that!(sut.as_bytes(), eq b"fuu-blaa-fu");
+}
+
+pub fn remove_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
+    let mut sut = Sut::new(b"a01234").unwrap();
+    let result = sut.remove(2);
+    assert_that!(result, eq Ok(Some(b'1')));
+
+    assert_that!(sut, len 5);
+    assert_that!(sut.as_bytes(), eq b"a0234");
+}
+
+pub fn remove_range_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
+    let mut sut = Sut::new(b"a01234567").unwrap();
+    let result = sut.remove_range(3, 3);
+    assert_that!(result, is_ok);
+
+    assert_that!(sut, len 6);
+    assert_that!(sut.as_bytes(), eq b"a01567");
+}
+
+pub fn retain_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
+    let mut sut = Sut::new(b"a01234567").unwrap();
+    let result = sut.retain(|c| c == b'4');
+    assert_that!(result, is_ok);
+
+    assert_that!(sut, len 8);
+    assert_that!(sut.as_bytes(), eq b"a0123567");
+}
+
+pub fn strip_prefix_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
+    let mut sut = Sut::new(b"a0123a4567").unwrap();
+    assert_that!(sut.strip_prefix(b"a0123"), eq Ok(true));
+    assert_that!(sut.as_bytes(), eq b"a4567");
+
+    assert_that!(sut.strip_prefix(b"a0123"), eq Ok(false));
+    assert_that!(sut.as_bytes(), eq b"a4567");
+
+    let result = sut.strip_prefix(b"a45");
+    if result.is_ok() {
+        assert_that!(sut.as_bytes(), eq b"67");
+    } else {
+        assert_that!(result.err().unwrap(), eq SemanticStringError::InvalidContent);
+    }
+}
+
+pub fn strip_suffix_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
+    let mut sut = Sut::new(b"a0123a4567").unwrap();
+    assert_that!(sut.strip_suffix(b"a4567"), eq Ok(true));
+    assert_that!(sut.as_bytes(), eq b"a0123");
+
+    assert_that!(sut.strip_suffix(b"a4567"), eq Ok(false));
+    assert_that!(sut.as_bytes(), eq b"a0123");
+
+    let result = sut.strip_suffix(b"a0123");
+    if result.is_ok() {
+        assert_that!(sut.as_bytes(), eq b"");
+    } else {
+        assert_that!(result.err().unwrap(), eq SemanticStringError::InvalidContent);
+    }
+}
+
+pub fn truncate_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
+    let mut sut = Sut::new(b"a01234567").unwrap();
+    assert_that!(sut.truncate(4), is_ok);
+
+    assert_that!(sut, len 4);
+    assert_that!(sut.as_bytes(), eq b"a012");
+
+    assert_that!(sut.truncate(6), is_ok);
+    assert_that!(sut, len 4);
+    assert_that!(sut.as_bytes(), eq b"a012");
+
+    let result = sut.truncate(0);
+    if result.is_ok() {
+        assert_that!(sut, is_empty);
+    } else {
+        assert_that!(result.err().unwrap(), eq SemanticStringError::InvalidContent);
+    }
+}
+
+pub fn invalid_utf8_characters_fail<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
+    let sut = Sut::new(&[b'a', b'b', 0xdf, 0xff]);
+    assert_that!(sut, is_err);
+
+    let sut = Sut::new(&[b'f', b'u', b'u', 0xff, 0xff]);
+    assert_that!(sut, is_err);
+}
+
+pub fn is_full_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
+    let sut = Sut::new(b"a01234567").unwrap();
+    assert_that!(sut.is_full(), eq false);
+}
+
+pub fn capacity_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
+    let sut = Sut::new(b"a01234567").unwrap();
+    assert_that!(sut.capacity(), eq CAPACITY);
+}
+
+pub fn insert_too_much_bytes_fails<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
+    let mut sut = Sut::new(b"a01234567").unwrap();
+    let mut bytes = vec![];
+    for _ in 0..8192 {
+        bytes.push(b'a')
     }
 
-    #[test]
-    fn new_name_with_illegal_char_is_illegal<
-        const CAPACITY: usize,
-        Sut: SemanticString<CAPACITY>,
-    >() {
-        let sut = Sut::new(b"hello \0.txt");
-        assert_that!(sut, is_err);
-    }
+    let result = sut.insert_bytes(0, &bytes);
+    assert_that!(result, is_err);
+    assert_that!(
+        result.err().unwrap(), eq
+        SemanticStringError::ExceedsMaximumLength
+    );
+}
 
-    #[test]
-    fn try_from_legal_str_succeeds<
-        const CAPACITY: usize,
-        Sut: SemanticString<CAPACITY> + TryFrom<&'static str>,
-    >() {
-        let sut = Sut::try_from("woohoo-md");
-        assert_that!(sut, is_ok);
-        let sut = unsafe { sut.unwrap_unchecked() };
+pub fn pop_until_empty_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
+    let mut sut = Sut::new(b"aaa").unwrap();
 
-        assert_that!(sut, len 9);
-        assert_that!(sut.as_bytes(), eq b"woohoo-md");
-    }
-
-    #[test]
-    fn try_from_illegal_str_fails<
-        const CAPACITY: usize,
-        Sut: SemanticString<CAPACITY> + TryFrom<&'static str>,
-    >() {
-        let sut = Sut::try_from("oh zero \0.txt");
-        assert_that!(sut, is_err);
-    }
-
-    #[test]
-    fn insert_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
-        let mut sut = Sut::new(b"hello").unwrap();
-        assert_that!(sut.insert(1, b't'), is_ok);
-
-        assert_that!(sut, len 6);
-        assert_that!(sut.as_bytes(), eq b"htello");
-    }
-
-    #[test]
-    fn insert_illegal_character_fails<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
-        let mut sut = Sut::new(b"hello").unwrap();
-        assert_that!(sut.insert(1, b'*'), is_err);
-
-        assert_that!(sut, len 5);
-        assert_that!(sut.as_bytes(), eq b"hello");
-    }
-
-    #[test]
-    fn insert_bytes_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
-        let mut sut = Sut::new(b"wld").unwrap();
-        assert_that!(sut.insert_bytes(1, b"or"), is_ok);
-
-        assert_that!(sut, len 5);
-        assert_that!(sut.as_bytes(), eq b"world");
-    }
-
-    #[test]
-    fn insert_bytes_with_illegal_character_fails<
-        const CAPACITY: usize,
-        Sut: SemanticString<CAPACITY>,
-    >() {
-        let mut sut = Sut::new(b"wld").unwrap();
-        assert_that!(sut.insert_bytes(1, b"o\0r"), is_err);
-
-        assert_that!(sut, len 3);
-        assert_that!(sut.as_bytes(), eq b"wld");
-    }
-
-    #[test]
-    fn pop_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
-        let mut sut = Sut::new(b"fuu-blaa-fuu").unwrap();
+    let mut do_pop = || {
         let result = sut.pop();
-        assert_that!(result.unwrap(), eq Some(b'u'));
-
-        assert_that!(sut, len 11);
-        assert_that!(sut.as_bytes(), eq b"fuu-blaa-fu");
-    }
-
-    #[test]
-    fn remove_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
-        let mut sut = Sut::new(b"a01234").unwrap();
-        let result = sut.remove(2);
-        assert_that!(result, eq Ok(Some(b'1')));
-
-        assert_that!(sut, len 5);
-        assert_that!(sut.as_bytes(), eq b"a0234");
-    }
-
-    #[test]
-    fn remove_range_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
-        let mut sut = Sut::new(b"a01234567").unwrap();
-        let result = sut.remove_range(3, 3);
-        assert_that!(result, is_ok);
-
-        assert_that!(sut, len 6);
-        assert_that!(sut.as_bytes(), eq b"a01567");
-    }
-
-    #[test]
-    fn retain_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
-        let mut sut = Sut::new(b"a01234567").unwrap();
-        let result = sut.retain(|c| c == b'4');
-        assert_that!(result, is_ok);
-
-        assert_that!(sut, len 8);
-        assert_that!(sut.as_bytes(), eq b"a0123567");
-    }
-
-    #[test]
-    fn strip_prefix_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
-        let mut sut = Sut::new(b"a0123a4567").unwrap();
-        assert_that!(sut.strip_prefix(b"a0123"), eq Ok(true));
-        assert_that!(sut.as_bytes(), eq b"a4567");
-
-        assert_that!(sut.strip_prefix(b"a0123"), eq Ok(false));
-        assert_that!(sut.as_bytes(), eq b"a4567");
-
-        let result = sut.strip_prefix(b"a45");
         if result.is_ok() {
-            assert_that!(sut.as_bytes(), eq b"67");
+            assert_that!(result.unwrap().unwrap(), eq b'a');
         } else {
             assert_that!(result.err().unwrap(), eq SemanticStringError::InvalidContent);
         }
+    };
+
+    do_pop();
+    do_pop();
+    do_pop();
+
+    if sut.is_empty() {
+        assert_that!(sut.pop().unwrap(), eq None);
     }
-
-    #[test]
-    fn strip_suffix_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
-        let mut sut = Sut::new(b"a0123a4567").unwrap();
-        assert_that!(sut.strip_suffix(b"a4567"), eq Ok(true));
-        assert_that!(sut.as_bytes(), eq b"a0123");
-
-        assert_that!(sut.strip_suffix(b"a4567"), eq Ok(false));
-        assert_that!(sut.as_bytes(), eq b"a0123");
-
-        let result = sut.strip_suffix(b"a0123");
-        if result.is_ok() {
-            assert_that!(sut.as_bytes(), eq b"");
-        } else {
-            assert_that!(result.err().unwrap(), eq SemanticStringError::InvalidContent);
-        }
-    }
-
-    #[test]
-    fn truncate_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
-        let mut sut = Sut::new(b"a01234567").unwrap();
-        assert_that!(sut.truncate(4), is_ok);
-
-        assert_that!(sut, len 4);
-        assert_that!(sut.as_bytes(), eq b"a012");
-
-        assert_that!(sut.truncate(6), is_ok);
-        assert_that!(sut, len 4);
-        assert_that!(sut.as_bytes(), eq b"a012");
-
-        let result = sut.truncate(0);
-        if result.is_ok() {
-            assert_that!(sut, is_empty);
-        } else {
-            assert_that!(result.err().unwrap(), eq SemanticStringError::InvalidContent);
-        }
-    }
-
-    #[test]
-    fn invalid_utf8_characters_fail<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
-        let sut = Sut::new(&[b'a', b'b', 0xdf, 0xff]);
-        assert_that!(sut, is_err);
-
-        let sut = Sut::new(&[b'f', b'u', b'u', 0xff, 0xff]);
-        assert_that!(sut, is_err);
-    }
-
-    #[test]
-    fn is_full_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
-        let sut = Sut::new(b"a01234567").unwrap();
-        assert_that!(sut.is_full(), eq false);
-    }
-
-    #[test]
-    fn capacity_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
-        let sut = Sut::new(b"a01234567").unwrap();
-        assert_that!(sut.capacity(), eq CAPACITY);
-    }
-
-    #[test]
-    fn insert_too_much_bytes_fails<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
-        let mut sut = Sut::new(b"a01234567").unwrap();
-        let mut bytes = vec![];
-        for _ in 0..8192 {
-            bytes.push(b'a')
-        }
-
-        let result = sut.insert_bytes(0, &bytes);
-        assert_that!(result, is_err);
-        assert_that!(
-            result.err().unwrap(), eq
-            SemanticStringError::ExceedsMaximumLength
-        );
-    }
-
-    #[test]
-    fn pop_until_empty_works<const CAPACITY: usize, Sut: SemanticString<CAPACITY>>() {
-        let mut sut = Sut::new(b"aaa").unwrap();
-
-        let mut do_pop = || {
-            let result = sut.pop();
-            if result.is_ok() {
-                assert_that!(result.unwrap().unwrap(), eq b'a');
-            } else {
-                assert_that!(result.err().unwrap(), eq SemanticStringError::InvalidContent);
-            }
-        };
-
-        do_pop();
-        do_pop();
-        do_pop();
-
-        if sut.is_empty() {
-            assert_that!(sut.pop().unwrap(), eq None);
-        }
-    }
-
-    #[instantiate_tests(<{FileName::max_len()}, FileName>)]
-    mod file_name {}
-
-    #[instantiate_tests(<64, RestrictedFileName::<64>>)]
-    mod restricted_file_name {}
-
-    #[instantiate_tests(<{Path::max_len()}, Path>)]
-    mod path {}
-
-    #[instantiate_tests(<{FilePath::max_len()}, FilePath>)]
-    mod file_path {}
-
-    #[instantiate_tests(<{UserName::max_len()}, UserName>)]
-    mod user_name {}
-
-    #[instantiate_tests(<{GroupName::max_len()}, GroupName>)]
-    mod group_name {}
-
-    #[instantiate_tests(<{Base64Url::max_len()}, Base64Url>)]
-    mod base64url {}
 }
