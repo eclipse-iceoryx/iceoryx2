3581f6577a106516ce1c0c89865dadfe54cdc159
Jeff Ithier <jeff@ithier.dev>
2026-03-16 13:26:56 +0100

    [#1300] Fix clippy warnings

--- iceoryx2-bb/system-types/tests-nostd/src/ipv4_address_tests.rs
+++ iceoryx2-bb/system-types/tests-nostd/src/ipv4_address_tests.rs
@@ -10,6 +10,8 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
+#![allow(clippy::disallowed_types)]
+
 extern crate iceoryx2_bb_loggers;
 
 use iceoryx2_bb_system_types_tests_common::ipv4_address_tests;
