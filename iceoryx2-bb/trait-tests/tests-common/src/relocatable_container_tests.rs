f723f302522893eeff94d6fef4b05a3471a80b7b
Jeff Ithier <jeff@ithier.dev>
2026-03-26 15:50:11 +0100

    [#1300] Use test_module in iceoryx2-bb-trait-tests

--- iceoryx2-bb/trait-tests/tests-common/src/relocatable_container_tests.rs
+++ iceoryx2-bb/trait-tests/tests-common/src/relocatable_container_tests.rs
@@ -12,34 +12,9 @@
 
 #![allow(clippy::disallowed_types)]
 
-use alloc::boxed::Box;
+use iceoryx2_bb_testing_macros::test_module;
 
-use core::ptr::NonNull;
-use iceoryx2_bb_container::{queue::RelocatableQueue, vector::relocatable_vec::RelocatableVec};
-use iceoryx2_bb_elementary_traits::relocatable_container::RelocatableContainer;
-use iceoryx2_bb_lock_free::{
-    mpmc::{container::Container, unique_index_set::UniqueIndexSet},
-    spsc::{
-        index_queue::RelocatableIndexQueue,
-        safely_overflowing_index_queue::RelocatableSafelyOverflowingIndexQueue,
-    },
-};
-use iceoryx2_bb_memory::bump_allocator::BumpAllocator;
-use iceoryx2_bb_testing::assert_that;
-use iceoryx2_bb_testing_macros::{inventory_test_generic, requires_std};
-use iceoryx2_cal::zero_copy_connection::used_chunk_list::RelocatableUsedChunkList;
-
-const MEMORY_SIZE: usize = 1024 * 128;
-
-fn memory() -> Box<[u8; MEMORY_SIZE]> {
-    Box::new([0u8; MEMORY_SIZE])
-}
-
-fn allocator(memory: &mut [u8]) -> BumpAllocator {
-    BumpAllocator::new(NonNull::new(memory.as_mut_ptr()).unwrap(), memory.len())
-}
-
-#[inventory_test_generic(
+#[test_module(
     RelocatableVec<u64>,
     RelocatableVec<u128>,
     RelocatableVec<[u8; 123]>,
@@ -54,82 +29,83 @@ fn allocator(memory: &mut [u8]) -> BumpAllocator {
     RelocatableSafelyOverflowingIndexQueue,
     RelocatableUsedChunkList
 )]
-pub fn init_acquires_less_or_equal_the_required_size_of_bytes<T: RelocatableContainer>() {
-    const MAX_CAPACITY: usize = 128;
+pub mod tests {
+    use alloc::boxed::Box;
+    use core::ptr::NonNull;
+    use iceoryx2_bb_container::{queue::RelocatableQueue, vector::relocatable_vec::RelocatableVec};
+    use iceoryx2_bb_elementary_traits::relocatable_container::RelocatableContainer;
+    use iceoryx2_bb_lock_free::{
+        mpmc::{container::Container, unique_index_set::UniqueIndexSet},
+        spsc::{
+            index_queue::RelocatableIndexQueue,
+            safely_overflowing_index_queue::RelocatableSafelyOverflowingIndexQueue,
+        },
+    };
+    use iceoryx2_bb_memory::bump_allocator::BumpAllocator;
+    use iceoryx2_bb_testing::assert_that;
+    use iceoryx2_bb_testing_macros::requires_std;
+    use iceoryx2_cal::zero_copy_connection::used_chunk_list::RelocatableUsedChunkList;
 
-    for capacity in 1..MAX_CAPACITY {
+    const MEMORY_SIZE: usize = 1024 * 128;
+
+    fn memory() -> Box<[u8; MEMORY_SIZE]> {
+        Box::new([0u8; MEMORY_SIZE])
+    }
+
+    fn allocator(memory: &mut [u8]) -> BumpAllocator {
+        BumpAllocator::new(NonNull::new(memory.as_mut_ptr()).unwrap(), memory.len())
+    }
+
+    #[inventory_test]
+    pub fn init_acquires_less_or_equal_the_required_size_of_bytes<T: RelocatableContainer>() {
+        const MAX_CAPACITY: usize = 128;
+
+        for capacity in 1..MAX_CAPACITY {
+            let mut memory = memory();
+            let allocator = allocator(&mut *memory);
+
+            let mut sut = unsafe { T::new_uninit(capacity) };
+            let require_memory_size = T::memory_size(capacity);
+
+            assert_that!(unsafe { sut.init(&allocator) }, is_ok);
+            assert_that!(allocator.used_space(), le require_memory_size);
+        }
+    }
+
+    #[inventory_test]
+    pub fn init_acquires_less_or_equal_the_required_size_of_bytes_multiple_allocations<
+        T: RelocatableContainer,
+    >() {
+        const MAX_CAPACITY: usize = 18;
+
         let mut memory = memory();
         let allocator = allocator(&mut *memory);
 
-        let mut sut = unsafe { T::new_uninit(capacity) };
-        let require_memory_size = T::memory_size(capacity);
+        let mut current_size = 0;
+        for capacity in 1..MAX_CAPACITY {
+            let mut sut = unsafe { T::new_uninit(capacity) };
+            let require_memory_size = T::memory_size(capacity);
+
+            assert_that!(unsafe { sut.init(&allocator) }, is_ok);
+            assert_that!(allocator.used_space(), le current_size + require_memory_size);
+            current_size = allocator.used_space();
+        }
+    }
+
+    #[inventory_test]
+    #[requires_std("panics")]
+    #[should_panic]
+    pub fn init_twice_causes_panic<T: RelocatableContainer>() {
+        const MAX_CAPACITY: usize = 18;
+
+        let mut memory = memory();
+        let allocator = allocator(&mut *memory);
+
+        let mut sut = unsafe { T::new_uninit(MAX_CAPACITY) };
 
         assert_that!(unsafe { sut.init(&allocator) }, is_ok);
-        assert_that!(allocator.used_space(), le require_memory_size);
-    }
-}
 
-#[inventory_test_generic(
-    RelocatableVec<u64>,
-    RelocatableVec<u128>,
-    RelocatableVec<[u8; 123]>,
-    RelocatableQueue<u64>,
-    RelocatableQueue<u128>,
-    RelocatableQueue<[u8; 123]>,
-    Container<u64>,
-    Container<u128>,
-    Container<[u8; 123]>,
-    UniqueIndexSet,
-    RelocatableIndexQueue,
-    RelocatableSafelyOverflowingIndexQueue,
-    RelocatableUsedChunkList
-)]
-pub fn init_acquires_less_or_equal_the_required_size_of_bytes_multiple_allocations<
-    T: RelocatableContainer,
->() {
-    const MAX_CAPACITY: usize = 18;
-
-    let mut memory = memory();
-    let allocator = allocator(&mut *memory);
-
-    let mut current_size = 0;
-    for capacity in 1..MAX_CAPACITY {
-        let mut sut = unsafe { T::new_uninit(capacity) };
-        let require_memory_size = T::memory_size(capacity);
-
+        //panics
         assert_that!(unsafe { sut.init(&allocator) }, is_ok);
-        assert_that!(allocator.used_space(), le current_size + require_memory_size);
-        current_size = allocator.used_space();
     }
-}
-
-#[inventory_test_generic(
-    RelocatableVec<u64>,
-    RelocatableVec<u128>,
-    RelocatableVec<[u8; 123]>,
-    RelocatableQueue<u64>,
-    RelocatableQueue<u128>,
-    RelocatableQueue<[u8; 123]>,
-    Container<u64>,
-    Container<u128>,
-    Container<[u8; 123]>,
-    UniqueIndexSet,
-    RelocatableIndexQueue,
-    RelocatableSafelyOverflowingIndexQueue,
-    RelocatableUsedChunkList
-)]
-#[requires_std("panics")]
-#[should_panic]
-pub fn init_twice_causes_panic<T: RelocatableContainer>() {
-    const MAX_CAPACITY: usize = 18;
-
-    let mut memory = memory();
-    let allocator = allocator(&mut *memory);
-
-    let mut sut = unsafe { T::new_uninit(MAX_CAPACITY) };
-
-    assert_that!(unsafe { sut.init(&allocator) }, is_ok);
-
-    //panics
-    assert_that!(unsafe { sut.init(&allocator) }, is_ok);
 }
