ebd831ae64bdabf84477595da6f68675aaa301a0
Mathias Kraus <elboberido@m-hias.de>
2026-03-27 03:21:36 +0100

    [#1300] Make iceoryx2-ffi-c no_std

--- iceoryx2-ffi/c/src/lib.rs
+++ iceoryx2-ffi/c/src/lib.rs
@@ -23,3 +23,50 @@ pub use api::*;
 
 #[cfg(test)]
 mod tests;
+
+#[cfg(not(feature = "std"))]
+mod no_std {
+    use alloc::alloc::{GlobalAlloc, Layout};
+    use iceoryx2_pal_posix::posix::{free, malloc};
+
+    struct LibcAllocator;
+
+    unsafe impl GlobalAlloc for LibcAllocator {
+        unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+            malloc(layout.size()) as *mut u8
+        }
+        unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
+            free(ptr as *mut core::ffi::c_void)
+        }
+    }
+
+    #[global_allocator]
+    static GLOBAL: LibcAllocator = LibcAllocator;
+
+    use core::panic::PanicInfo;
+    use iceoryx2_bb_posix::signal::SignalHandler;
+    use iceoryx2_bb_print::coutln;
+
+    #[panic_handler]
+    pub fn panic(info: &PanicInfo) -> ! {
+        coutln!("");
+        coutln!("╔═══════════════════════════════════════╗");
+        coutln!("║           PANIC OCCURRED!             ║");
+        coutln!("╚═══════════════════════════════════════╝");
+
+        if let Some(location) = info.location() {
+            coutln!("Location: {}:{}\n", location.file(), location.line());
+        }
+
+        coutln!("Message: {}\n", info);
+
+        SignalHandler::abort();
+
+        loop {
+            core::hint::spin_loop();
+        }
+    }
+
+    #[no_mangle]
+    pub extern "C" fn rust_eh_personality() {}
+}
