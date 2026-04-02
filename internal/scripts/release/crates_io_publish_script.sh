aaea99fb855f01917d324116c3b4dc2fe81b6f37
Mathias Kraus <elboberido@m-hias.de>
2026-04-02 22:50:27 +0200

    [#1300] Add no_std tests for iceoryx2-linux-tests

--- internal/scripts/release/crates_io_publish_script.sh
+++ internal/scripts/release/crates_io_publish_script.sh
@@ -81,6 +81,8 @@ CRATES_TO_IGNORE=(
     iceoryx2-bb-threadsafe-tests-nostd
     iceoryx2-bb-concurrency-tests-common
     iceoryx2-bb-concurrency-tests-nostd
+    iceoryx2-bb-linux-tests-common
+    iceoryx2-bb-linux-tests-nostd
     iceoryx2-bb-lock-free-tests-common
     iceoryx2-bb-lock-free-tests-nostd
     iceoryx2-bb-memory-tests-common
