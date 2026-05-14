d39ef82de2b3a677b3697de678a9ae6f21572174
MikuSoft <1418561105@qq.com>
2026-05-14 15:47:19 +0800

    [eclipse-iceoryx#1215] Drop python requirement to 3.8

--- iceoryx2-ffi/python/python-src/iceoryx2/publish_subscribe_extensions.py
+++ iceoryx2-ffi/python/python-src/iceoryx2/publish_subscribe_extensions.py
@@ -44,7 +44,8 @@ def publish_subscribe(
     self: ServiceBuilder, t: Type[T]
 ) -> ServiceBuilderPublishSubscribe:
     """Returns the `ServiceBuilderPublishSusbcribe` to create a new publish-subscribe service. The payload ctype must be provided as argument."""
-    type_name = t.__name__
+    type_name = getattr(t, "__name__", None)
+    type_name = type_name or get_origin(t).__name__
     type_size = 0
     type_align = 0
     type_variant = TypeVariant.FixedSize
