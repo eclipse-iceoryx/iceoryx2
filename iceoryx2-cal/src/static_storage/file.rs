c1722dc712faf99dbf0e81eebd24bbaafee247b9
Christian Eltzschig <me@elchris.org>
2026-05-25 16:51:40 +0200

    [#1675] Directly remove concept in remove_cfg

--- iceoryx2-cal/src/static_storage/file.rs
+++ iceoryx2-cal/src/static_storage/file.rs
@@ -260,43 +260,16 @@ impl crate::named_concept::NamedConceptMgmt for Storage {
 
         let file_path = config.path_for(storage_name);
 
-        let mut file = match FileBuilder::new(&file_path).open_existing(AccessMode::Read) {
-            Ok(f) => f,
-            Err(FileOpenError::FileDoesNotExist) => return Ok(false),
-            Err(FileOpenError::InsufficientPermissions) => {
-                fail!(from origin, with NamedConceptRemoveError::InsufficientPermissions,
-                    "{} since the file could not be opened for permission adjustment.", msg);
-            }
-            Err(FileOpenError::Interrupt) => {
-                fail!(from origin, with NamedConceptRemoveError::Interrupt,
-                    "{} since an interrupt signal was raised.", msg);
-            }
-            Err(v) => {
-                fail!(from origin, with NamedConceptRemoveError::InternalError,
-                    "{} since the file could not be opened due to an unknown failure [{:?}].", msg, v);
-            }
-        };
-
-        let set_permission_result = file.set_permission(Permission::ALL);
-
         match File::remove(&file_path) {
             Ok(v) => Ok(v),
-            Err(e) => {
-                if let Err(permission_result) = set_permission_result {
-                    warn!(from origin,
-                          "Unable to adjust the files permission as preparation to remove the file ({permission_result:?}).");
-                }
-                match e {
-                    FileRemoveError::InsufficientPermissions
-                    | FileRemoveError::PartOfReadOnlyFileSystem => {
-                        fail!(from origin, with NamedConceptRemoveError::InsufficientPermissions,
+            Err(FileRemoveError::InsufficientPermissions)
+            | Err(FileRemoveError::PartOfReadOnlyFileSystem) => {
+                fail!(from origin, with NamedConceptRemoveError::InsufficientPermissions,
                                 "{} due to insufficient permissions.", msg);
-                    }
-                    e => {
-                        fail!(from origin, with NamedConceptRemoveError::InternalError,
+            }
+            Err(e) => {
+                fail!(from origin, with NamedConceptRemoveError::InternalError,
                                 "{} due to unknown failure ({:?}).", msg, e);
-                    }
-                }
             }
         }
     }
