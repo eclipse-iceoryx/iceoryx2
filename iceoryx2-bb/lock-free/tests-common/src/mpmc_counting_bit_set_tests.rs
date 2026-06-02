2b9368cd2d4513366614c8f664dea13b073a0595
Christian Eltzschig <me@elchris.org>
2026-06-02 15:20:02 +0200

    [#925] Add concurrency test

--- iceoryx2-bb/lock-free/tests-common/src/mpmc_counting_bit_set_tests.rs
+++ iceoryx2-bb/lock-free/tests-common/src/mpmc_counting_bit_set_tests.rs
@@ -10,8 +10,16 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
+use core::time::Duration;
+
+use iceoryx2_bb_concurrency::atomic::{AtomicBool, AtomicU64, AtomicUsize, Ordering};
 use iceoryx2_bb_lock_free::mpmc::counting_bit_set::{CountingBitSet, FixedSizeCountingBitSet};
+use iceoryx2_bb_posix::barrier::{BarrierBuilder, BarrierHandle, Handle};
+use iceoryx2_bb_posix::clock::nanosleep;
+use iceoryx2_bb_posix::system_configuration::SystemInfo;
+use iceoryx2_bb_posix::thread::thread_scope;
 use iceoryx2_bb_testing::assert_that;
+use iceoryx2_bb_testing::watchdog::Watchdog;
 use iceoryx2_bb_testing_macros::test;
 
 const SUT_CAPACITY: usize = 32;
@@ -105,5 +113,87 @@ pub fn heap_based_counting_bitset_works() {
             assert_that!(state.count(), eq 1);
         });
         assert_that!(callback_counter, eq 1);
+    }
+}
+
+#[test]
+pub fn concurrent_set_and_reset_works() {
+    let _watchdog = Watchdog::new();
+
+    let number_of_set_threads = (SystemInfo::NumberOfCpuCores.value() / 2).clamp(2, usize::MAX);
+    let number_of_reset_threads = (SystemInfo::NumberOfCpuCores.value() / 2).clamp(2, usize::MAX);
+    const CAPACITY: usize = 10;
+    const ITERATIONS: usize = 100000;
+
+    let sut = CountingBitSet::new(CAPACITY);
+    let start_barrier_handle = BarrierHandle::new();
+    let start_barrier =
+        BarrierBuilder::new((number_of_set_threads + number_of_reset_threads + 1) as u32)
+            .create(&start_barrier_handle)
+            .unwrap();
+
+    let set_count = [const { AtomicU64::new(0) }; CAPACITY];
+    let reset_count = [const { AtomicU64::new(0) }; CAPACITY];
+
+    let keep_running = AtomicBool::new(true);
+    let number_of_completed_set_threads = AtomicUsize::new(0);
+
+    thread_scope(|s| {
+        for _ in 0..number_of_set_threads {
+            s.thread_builder()
+                .spawn(|| {
+                    let mut counter = 0;
+                    let mut id_counter = [0u64; CAPACITY];
+
+                    start_barrier.wait();
+                    while counter < ITERATIONS {
+                        sut.set(counter % CAPACITY);
+                        id_counter[counter % CAPACITY] += 1;
+                        counter += 1;
+                    }
+
+                    for (idx, count) in id_counter.iter().enumerate() {
+                        set_count[idx].fetch_add(*count, Ordering::Relaxed);
+                    }
+
+                    number_of_completed_set_threads.fetch_add(1, Ordering::SeqCst);
+                })
+                .expect("failed to spawn thread");
+        }
+
+        for _ in 0..number_of_reset_threads {
+            s.thread_builder()
+                .spawn(|| {
+                    let mut id_counter = [0u64; CAPACITY];
+
+                    start_barrier.wait();
+                    while keep_running.load(Ordering::SeqCst) {
+                        sut.reset_all(|state| {
+                            id_counter[state.bit()] += state.count();
+                        });
+                    }
+                    sut.reset_all(|state| {
+                        id_counter[state.bit()] += state.count();
+                    });
+
+                    for (idx, count) in id_counter.iter().enumerate() {
+                        reset_count[idx].fetch_add(*count, Ordering::Relaxed);
+                    }
+                })
+                .expect("failed to spawn thread");
+        }
+
+        start_barrier.wait();
+        while number_of_completed_set_threads.load(Ordering::Relaxed) < number_of_set_threads {
+            nanosleep(Duration::from_millis(1)).expect("sleep failed");
+        }
+        keep_running.store(false, Ordering::SeqCst);
+
+        Ok(())
+    })
+    .expect("failed to run thread scope");
+
+    for i in 0..CAPACITY {
+        assert_that!(set_count[i].load(Ordering::Relaxed), eq reset_count[i].load(Ordering::Relaxed));
     }
 }
