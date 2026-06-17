e1fa5896e6ac883a650f2ff120bf88e57124567e
Christian Eltzschig <me@elchris.org>
2026-06-17 10:55:11 +0200

    [#1745] Implement snake and camel case converter

--- iceoryx2-bb/elementary/src/code_style.rs
+++ iceoryx2-bb/elementary/src/code_style.rs
@@ -15,9 +15,41 @@ extern crate alloc;
 use alloc::string::String;
 
 pub fn snake_to_camel_case(input: &str) -> String {
-    todo!()
+    let mut result = String::new();
+    let mut capitalize_next = false;
+    for c in input.chars() {
+        if c == '_' {
+            capitalize_next = true;
+        } else if capitalize_next {
+            for upper_c in c.to_uppercase() {
+                result.push(upper_c);
+            }
+            capitalize_next = false;
+        } else {
+            result.push(c);
+        }
+    }
+    result
 }
 
 pub fn camel_to_snake_case(input: &str) -> String {
-    todo!()
+    let mut result = String::new();
+    let mut prev_char: Option<char> = None;
+
+    for c in input.chars() {
+        if c.is_uppercase() {
+            if let Some(prev) = prev_char {
+                if prev.is_lowercase() || prev.is_numeric() {
+                    result.push('_');
+                }
+            }
+            for lower_c in c.to_lowercase() {
+                result.push(lower_c);
+            }
+        } else {
+            result.push(c);
+        }
+        prev_char = Some(c);
+    }
+    result
 }
