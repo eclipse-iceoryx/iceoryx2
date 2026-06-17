18426475c49e284cd2da8802644d440b3578e006
Christian Eltzschig <me@elchris.org>
2026-06-17 20:36:01 +0200

    [#1745] Add flatbuffer schema path to service builder

--- examples/rust/flatbuffer/publisher.rs
+++ examples/rust/flatbuffer/publisher.rs
@@ -28,10 +28,12 @@ fn main() -> Result<(), Box<dyn core::error::Error>> {
     let node = NodeBuilder::new().create::<ipc::Service>()?;
 
     println!("type name: {}", core::any::type_name::<UnboundedData>());
+    println!("{}", core::env!("PWD"));
 
     let service = node
         .service_builder(&"My/Flatbuffer/Service".try_into()?)
         .publish_subscribe::<Flatbuffer<UnboundedData>>()
+        .flatbuffer_schema_path(&"unbounded_data.fbs".try_into()?)
         .open_or_create()?;
 
     let publisher = service
