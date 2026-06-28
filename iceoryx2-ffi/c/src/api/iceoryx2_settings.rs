c620d06f99af3aa656504fc653064ee2b93b0027
Mathias Kraus <elboberido@m-hias.de>
2026-06-28 20:36:23 +0200

    [#1773] Add C binding for 'PortName'

--- iceoryx2-ffi/c/src/api/iceoryx2_settings.rs
+++ iceoryx2-ffi/c/src/api/iceoryx2_settings.rs
@@ -14,6 +14,7 @@ pub const IOX2_ATTRIBUTE_KEY_LENGTH: usize = 64;
 pub const IOX2_ATTRIBUTE_VALUE_LENGTH: usize = 256;
 pub const IOX2_MAX_ATTRIBUTES_PER_SERVICE: usize = 8;
 pub const IOX2_NODE_NAME_LENGTH: usize = 128;
+pub const IOX2_PORT_NAME_LENGTH: usize = 64;
 pub const IOX2_SERVICE_NAME_LENGTH: usize = 255;
 pub const IOX2_SERVICE_HASH_LENGTH: usize = 64;
 pub const IOX2_TYPE_NAME_LENGTH: usize = 256;
