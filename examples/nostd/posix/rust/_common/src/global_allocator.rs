3e96d3ad5309b05960ee88b54d2793c3ecad16ef
Christian Eltzschig <me@elchris.org>
2026-07-24 11:58:05 +0200

    [#1745] Fix no_std allocator

--- examples/nostd/posix/rust/_common/src/global_allocator.rs
+++ examples/nostd/posix/rust/_common/src/global_allocator.rs
@@ -16,6 +16,7 @@ use core::{
 };
 
 use iceoryx2_bb_elementary_traits::allocator::BaseAllocator;
+use iceoryx2_bb_elementary_traits::allocator::Dealloc;
 use iceoryx2_bb_memory::heap_allocator::HeapAllocator;
 
 #[derive(Debug)]
