ce7fbdbe410d599b930815e376e90c744a949143
Christian Eltzschig <me@elchris.org>
2026-07-25 12:01:42 +0200

    [#1745] Add flatbuffer c++ example skeleton

---
+++ examples/cxx/flatbuffer_publish_subscribe/src/publisher.cpp
@@ -0,0 +1,50 @@
+// Copyright (c) 2024 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache Software License 2.0 which is available at
+// https://www.apache.org/licenses/LICENSE-2.0, or the MIT license
+// which is available at https://opensource.org/licenses/MIT.
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+#include <cstdint>
+#include <iostream>
+#include <utility>
+
+#include "iox2/iceoryx2.hpp"
+#include "unbounded_data_generated.h"
+
+constexpr iox2::bb::Duration CYCLE_TIME = iox2::bb::Duration::from_secs(1);
+
+auto main() -> int {
+    using namespace iox2;
+    set_log_level_from_env_or(LogLevel::Info);
+    auto node = NodeBuilder().create<ServiceType::Ipc>().value();
+
+    auto service = node.service_builder(ServiceName::create("My/Flatbuffer/Service").value())
+                       .publish_subscribe<uint64_t>()
+                       .open_or_create()
+                       .value();
+
+    auto publisher = service.publisher_builder().create().value();
+
+    auto counter = 0;
+    while (node.wait(CYCLE_TIME).has_value()) {
+        counter += 1;
+
+        auto sample = publisher.loan_uninit().value();
+
+        auto initialized_sample = sample.write_payload(1);
+
+        send(std::move(initialized_sample)).has_value();
+
+        std::cout << "Send sample " << counter << "..." << std::endl;
+    }
+
+    std::cout << "exit" << std::endl;
+
+    return 0;
+}
