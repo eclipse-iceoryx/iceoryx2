56cadaa34aed959b3279be24a6c77fadcf1eb345
Christian Eltzschig <me@elchris.org>
2026-07-30 18:21:19 +0200

    [#1745] Create python example skeleton

---
+++ examples/python/flatbuffer_publish_subscribe/publisher.py
@@ -0,0 +1,45 @@
+# Copyright (c) 2025 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache Software License 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0, or the MIT license
+# which is available at https://opensource.org/licenses/MIT.
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+
+"""Publisher example."""
+
+import Example
+
+import iceoryx2 as iox2
+
+cycle_time = iox2.Duration.from_secs(1)
+
+iox2.set_log_level_from_env_or(iox2.LogLevel.Info)
+node = iox2.NodeBuilder.new().create(iox2.ServiceType.Ipc)
+
+# service = (
+#     node.service_builder(iox2.ServiceName.new("My/Flatbuffer/Service"))
+#     .publish_subscribe(TransmissionData)
+#     .open_or_create()
+# )
+
+# publisher = service.publisher_builder().create()
+
+COUNTER = 0
+try:
+    while True:
+        COUNTER += 1
+        node.wait(cycle_time)
+        # sample = publisher.loan_uninit()
+        # sample = sample.write_payload(
+        #     TransmissionData(x=COUNTER, y=COUNTER * 3, funky=COUNTER * 812.12)
+        # )
+        # sample.send()
+        print("Send sample", COUNTER, "...")
+
+except iox2.NodeWaitFailure:
+    print("exit")
