0604772a09c6e486bfa7267f868dc82c54a38cf4
Christian Eltzschig <me@elchris.org>
2026-08-03 10:40:12 +0200

    [#1745] Add first flatbuffer rpc example

---
+++ examples/rust/flatbuffer_request_response/README.md
@@ -0,0 +1,1 @@
+# FlatBuffers Request-Response
---
+++ examples/rust/flatbuffer_request_response/server.rs
@@ -0,0 +1,65 @@
+// Copyright (c) 2025 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache Software License 2.0 which is available at
+// https://www.apache.org/licenses/LICENSE-2.0, or the MIT license
+// which is available at https://opensource.org/licenses/MIT.
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+use core::time::Duration;
+use iceoryx2::prelude::*;
+
+use crate::{
+    data_props_generated::example::DataProps, unbounded_data_generated::example::UnboundedData,
+};
+
+#[path = "unbounded_data_generated.rs"]
+#[allow(clippy::all)]
+#[rustfmt::skip]
+mod unbounded_data_generated;
+
+#[path = "data_props_generated.rs"]
+#[allow(clippy::all)]
+#[rustfmt::skip]
+mod data_props_generated;
+
+const CYCLE_TIME: Duration = Duration::from_millis(100);
+
+fn main() -> Result<(), Box<dyn core::error::Error>> {
+    set_log_level_from_env_or(LogLevel::Info);
+
+    // export IOX2_FLATBUFFER_SCHEMA_PATH=${pwd}/examples/rust/flatbuffer_request_response
+    let lookup_path = std::env::var("IOX2_FLATBUFFER_SCHEMA_PATH")
+        .inspect_err(|e| cerrln!("Please define IOX2_FLATBUFFER_SCHEMA_PATH! [{e:?}]"))?;
+
+    let mut config = Config::default();
+    config.global.service.flatbuffer_schema_path = Some(lookup_path.as_str().try_into()?);
+
+    let node = NodeBuilder::new()
+        // Use the config with the defined flatbuffer schema path to enable automatic flatbuffer
+        // schema file lookup.
+        .config(&config)
+        .create::<ipc::Service>()?;
+
+    //let service = node
+    //    .service_builder(&"My/Funk/ServiceName".try_into()?)
+    //    .request_response::<Flatbuffer<UnboundedData>, Flatbuffer<DataProps>>()
+    //    .open_or_create()?;
+
+    //let server = service.server_builder().create()?;
+
+    coutln!("Server ready to receive requests!");
+
+    let mut counter = 0;
+    while node.wait(CYCLE_TIME).is_ok() {
+        counter += 1;
+    }
+
+    coutln!("exit");
+
+    Ok(())
+}
