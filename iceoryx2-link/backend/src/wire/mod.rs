89854a884826a1ada5702643636bc8c8fd76cf0b
Jeff Ithier <jeff@ithier.dev>
2026-09-13 17:57:13 +0200

    [#1977] Encode samples and notifications for the wire

--- iceoryx2-link/backend/src/wire/mod.rs
+++ iceoryx2-link/backend/src/wire/mod.rs
@@ -10,17 +10,5 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-#![no_std]
-
-extern crate alloc;
-
-/// The origin of a log line, `site` under the module it is logged from.
-#[macro_export]
-macro_rules! origin {
-    ($site:literal) => {
-        concat!(module_path!(), "::", $site)
-    };
-}
-
-pub mod description;
-pub mod wire;
+pub mod event;
+pub mod publish_subscribe;
